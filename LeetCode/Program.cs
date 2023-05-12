1931e2e58921edd4d1aafd5672d19c6fd8f47324
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-12 16:20:36 +0300

    Third Assignment
    Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
    
    The overall run time complexity should be O(log (m+n)).
    
    Example 1:
    
    Input: nums1 = [1,3], nums2 = [2]
    Output: 2.00000
    Explanation: merged array = [1,2,3] and median is 2.
    
    Example 2:
    
    Input: nums1 = [1,2], nums2 = [3,4]
    Output: 2.50000
    Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,31 +1,11 @@
-﻿namespace LeetCode
+﻿using System.Runtime.CompilerServices;
+
+namespace LeetCode
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] l1 = { 2, 4, 3 };
-            int[] l2 = { 5, 6, 4 };
-
-            Array.Reverse(l1);
-            Array.Reverse(l2);
-            string f1 = "";
-            string f2 = "";
-
-            foreach(int i in l1)
-            {
-                f1 += i.ToString();
-            }
-            foreach(int i in l2)
-            {
-                f2 += i.ToString();
-            }
-            Console.WriteLine(Int32.Parse(f1) + Int32.Parse(f2));
-            int[] result = (Int32.Parse(f1) + Int32.Parse(f2)).ToString().Select(o => Convert.ToInt32(o) - 48).ToArray();
-            foreach(int i in result)
-            {
-                Console.WriteLine(i);
-            }
 
         }
     }
---
+++ LeetCode/ThirdAssignment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class ThirdAssignment
+{
+    public class Solution3
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] nums3 = new int[nums1.Length + nums2.Length];
+
+            nums1.CopyTo(nums3, 0);
+            nums2.CopyTo(nums3, nums1.Length);
+
+            int n = nums3.Length;
+            Array.Sort(nums3);
+
+            if (n % 2 != 0)
+                return (double)nums3[n / 2];
+
+            return (double)(nums3[(n - 1) / 2] + nums3[n / 2]) / 2.0;
+        }
+    }
+}
