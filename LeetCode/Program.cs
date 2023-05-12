bcf3210ca608a4427ba97fbdc9d7d46927003372
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-12 13:53:42 +0300

    Second Assignment
    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
    
    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
    
    Input: l1 = [2,4,3], l2 = [5,6,4]
    Output: [7,0,8]
    Explanation: 342 + 465 = 807.
    
    Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
    Output: [8,9,9,9,0,0,0,1]

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] l1 = { 2, 4, 3 };
+            int[] l2 = { 5, 6, 4 };
+
+            Array.Reverse(l1);
+            Array.Reverse(l2);
+            string f1 = "";
+            string f2 = "";
+
+            foreach(int i in l1)
+            {
+                f1 += i.ToString();
+            }
+            foreach(int i in l2)
+            {
+                f2 += i.ToString();
+            }
+            Console.WriteLine(Int32.Parse(f1) + Int32.Parse(f2));
+            int[] result = (Int32.Parse(f1) + Int32.Parse(f2)).ToString().Select(o => Convert.ToInt32(o) - 48).ToArray();
+            foreach(int i in result)
+            {
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
