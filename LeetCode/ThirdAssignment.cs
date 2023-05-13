6fb7b43b49b48d7ea85b4a92c11c8e1308e1c364
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-13 11:10:09 +0300

    Third Assignment
    Given a string s, find the length of the longest
    substring
     without repeating characters.
    
    Example 1:
    
    Input: s = "abcabcbb"
    Output: 3
    Explanation: The answer is "abc", with the length of 3.

---
+++ LeetCode/ThirdAssignment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ThirdAssignment
+{
+    public class Solution4
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return 0;
+            var map_str = new Dictionary<char, int>();
+            var max_len = 0;
+            var last_repeat_pos = -1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (map_str.ContainsKey(s[i]) && last_repeat_pos < map_str[s[i]])
+                    last_repeat_pos = map_str[s[i]];
+                if (max_len < i - last_repeat_pos)
+                    max_len = i - last_repeat_pos;
+                map_str[s[i]] = i;
+            }
+            return max_len;
+        }
+    }
+}
