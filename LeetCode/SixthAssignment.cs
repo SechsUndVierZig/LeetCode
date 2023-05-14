f5f1d896e5623179e11c017b0de3366192f408ac
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-14 13:22:51 +0300

    SixthAssignment
    The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
    
    P   A   H   N
    A P L S I I G
    Y   I   R
    And then read line by line: "PAHNAPLSIIGYIR"
    
    Write the code that will take a string and make this conversion given a number of rows:
    
    string convert(string s, int numRows);

---
+++ LeetCode/SixthAssignment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class SixthAssignment
+{
+    public class Solution
+    {
+        public string Convert(string s, int numRows)
+        {
+            var arr = new char[s.Length];
+            int index = 0;
+            int rowIndex = 0;
+            bool goingDown = true;
+
+            if (numRows == 1)
+                return s;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                arr[i] = s[index];
+                if (goingDown)
+                    index += (numRows - rowIndex - 1) * 2;
+                else
+                    index += rowIndex * 2;
+
+                if (rowIndex != 0)
+                    goingDown = !goingDown;
+                if (index >= s.Length)
+                {
+                    rowIndex++;
+                    index = rowIndex;
+                    goingDown = true;
+                    if (rowIndex == numRows - 1)
+                        rowIndex = 0;
+                }
+            }
+
+            return new string(arr);
+        }
+    }
+}
