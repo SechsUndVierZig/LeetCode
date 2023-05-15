3b843b69e4c11ce8f680db50be32cbef71268f75
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-15 14:53:22 +0300

    Assignment10
    Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
    
    '.' Matches any single character.​​​​
    '*' Matches zero or more of the preceding element.
    The matching should cover the entire input string (not partial).
    
    Example 1:
    
    Input: s = "aa", p = "a"
    Output: false
    Explanation: "a" does not match the entire string "aa".
    Example 2:
    
    Input: s = "aa", p = "a*"
    Output: true
    Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
    Example 3:
    
    Input: s = "ab", p = ".*"
    Output: true
    Explanation: ".*" means "zero or more (*) of any character (.)".

---
+++ LeetCode/Assignment10.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Assignment10
+{
+    public class Solution
+    {
+        public bool IsMatch(string s, string p)
+        {
+            bool[][] dp = new bool[s.Length + 1][];
+            for (int i = 0; i < s.Length + 1; i++)
+                dp[i] = new bool[p.Length + 1];
+            dp[0][0] = true;
+
+            for (int j = 1; j < p.Length + 1; j++)
+                dp[0][j] = p[j - 1] == '*' && dp[0][j - 2];
+
+            for (int i = 1; i < s.Length + 1; i++)
+            {
+                for (int j = 1; j < p.Length + 1; j++)
+                {
+                    if (s[i - 1] == p[j - 1] || p[j - 1] == '.')
+                        dp[i][j] = dp[i - 1][j - 1];
+                    else if (p[j - 1] == '*')
+                        dp[i][j] = dp[i][j - 2] || (s[i - 1] == p[j - 2] || p[j - 2] == '.') && dp[i - 1][j];
+                }
+            }
+            return dp[s.Length][p.Length];
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 namespace LeetCode
 {
     internal class Program
     {
         static void Main(string[] args)
-        {                      
+        {
 
         }
     }
