7169a6d7c844761bb717b08e7f79deb2db9bce84
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-15 14:39:00 +0300

    Assignment9
    Given an integer x, return true if x is a
    palindrome
    , and false otherwise.
    
    Example 1:
    
    Input: x = 121
    Output: true
    Explanation: 121 reads as 121 from left to right and from right to left.
    Example 2:
    
    Input: x = -121
    Output: false
    Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
    Example 3:
    
    Input: x = 10
    Output: false
    Explanation: Reads 01 from right to left. Therefore it is not a palindrome.

---
+++ LeetCode/Assignment9.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Assignment9
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            string firstNumber = x.ToString();
+            char[] stringArray = firstNumber.ToCharArray();
+            Array.Reverse(stringArray);
+            string reversedStr = new string(stringArray);
+
+            return firstNumber == reversedStr ? true : false;
+
+        }
+    }
+}
