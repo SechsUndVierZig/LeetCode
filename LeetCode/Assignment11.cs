31668ede81a5c5339592bf2a17ebf99f75ae0621
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-15 14:59:58 +0300

    Assignment11
    You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
    
    Find two lines that together with the x-axis form a container, such that the container contains the most water.
    
    Return the maximum amount of water a container can store.
    
    Notice that you may not slant the container.
    
    Example 1:
    
    Input: height = [1,8,6,2,5,4,8,3,7]
    Output: 49
    Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
    Example 2:
    
    Input: height = [1,1]
    Output: 1

---
+++ LeetCode/Assignment11.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Assignment11
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            if (height == null || height.Length == 0) return 0;
+            int max = 0, i = 0, j = height.Length - 1;
+            while (i < j)
+            {
+                int h = Math.Min(height[i], height[j]);
+                int width = j - i;
+                max = Math.Max(max, h * width);
+                if (height[i] < height[j]) i++;
+                else j--;
+            }
+            return max;
+        }
+    }
+}
