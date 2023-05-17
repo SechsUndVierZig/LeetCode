27d3f54f4463ca5432646bba7218b276513d092e
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-17 15:46:26 +0300

    Assignment16
    Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
    
    Return the sum of the three integers.
    
    You may assume that each input would have exactly one solution.
    
    Example 1:
    
    Input: nums = [-1,2,1,-4], target = 1
    Output: 2
    Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
    Example 2:
    
    Input: nums = [0,0,0], target = 1
    Output: 0
    Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).

---
+++ LeetCode/Assignment16.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Assisgnment16
+{
+    public class Solution
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            var result = 0;
+            var closestDifference = int.MaxValue;
+            Array.Sort(nums);
+
+            for (int index = 0; index < nums.Length; ++index)
+            {
+                if (index > 0 && nums[index] == nums[index - 1]) continue;
+
+                var startIndex = index + 1;
+                var endIndex = nums.Length - 1;
+                while (startIndex < endIndex)
+                {
+                    var sum = nums[index] + nums[startIndex] + nums[endIndex];
+
+                    var difference = Math.Abs(target - sum);
+                    if (difference < closestDifference)
+                    {
+                        closestDifference = difference;
+                        result = sum;
+                    }
+                    if (difference == 0) break;
+                    else if (sum < target) ++startIndex;
+                    else --endIndex;
+                }
+                if (closestDifference == 0) break;
+            }
+            return result;
+        }
+    }
+}
