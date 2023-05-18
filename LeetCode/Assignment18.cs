15d83d669e833353b7288bcd998cdb55d15c1491
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-18 14:58:24 +0300

    Assignment18
    Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
    
    0 <= a, b, c, d < n
    a, b, c, and d are distinct.
    nums[a] + nums[b] + nums[c] + nums[d] == target
    You may return the answer in any order.
    
    Example 1:
    
    Input: nums = [1,0,-1,0,-2,2], target = 0
    Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
    Example 2:
    
    Input: nums = [2,2,2,2,2], target = 8
    Output: [[2,2,2,2]]

---
+++ LeetCode/Assignment18.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment18
+{
+    public class Solution
+    {
+        public IList<IList<int>> FourSum(int[] nums, long target)
+        {
+            var result = new List<IList<int>>();
+
+            Array.Sort(nums);
+            if (nums != null && nums[0] > 0 && nums[0] > target) return result;
+  
+            for (int i = 0; i < nums.Length - 3; i++)
+            {
+                while (i > 0 && i <= nums.Length - 3 && nums[i] == nums[i - 1])
+                {
+                    i++;
+                }
+                for (int j = i + 1; j < nums.Length - 2; j++)
+                {
+                    while (j > i + 1 && j <= nums.Length - 2 && nums[j] == nums[j - 1]) j++;
+
+                    long k = target - (nums[i] + nums[j]);
+                    long low = j + 1, high = nums.Length - 1;
+
+                    while (low < high)
+                    {
+                        long diff = nums[low] + nums[high];
+
+                        if (diff < k) low++;
+
+                        else if (diff > k) high--;
+
+                        else
+                        {
+                            long sum = nums[i] + nums[j] + nums[low] + nums[high];
+                            if (sum == target)
+                            {
+                                result.Add(new List<int>() { nums[i], nums[j], nums[low], nums[high] });
+                                low++;
+                                high--;
+                            }
+                            while (low < high && nums[low] == nums[low - 1]) low++;
+                            while (low < high && nums[high] == nums[high + 1]) high--;
+
+                        }
+                    }
+                }
+            }
+
+            return result;
+
+        }
+    }
