7b09569ee36f096f64560a22c9c7fa8371065a3e
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-18 15:42:28 +0300

    Assignment20
    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
    
    An input string is valid if:
    
    Open brackets must be closed by the same type of brackets.
    Open brackets must be closed in the correct order.
    Every close bracket has a corresponding open bracket of the same type.
    
    Example 1:
    
    Input: s = "()"
    Output: true
    Example 2:
    
    Input: s = "()[]{}"
    Output: true
    Example 3:
    
    Input: s = "(]"
    Output: false

---
+++ LeetCode/Assignment20.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Assignment20
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            int n = -1;
+            while (s.Length != n)
+            {
+                n = s.Length;
+                s = s.Replace("()", "");
+                s = s.Replace("{}", "");
+                s = s.Replace("[]", "");
+            }
+            if (s.Length == 0) return true;
+            else return false;
+        }
+    }
+}
