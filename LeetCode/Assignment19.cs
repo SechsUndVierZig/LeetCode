c2af0074924a33a200164b72f29fa662656c7430
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-18 15:13:56 +0300

    Assignment19
    Given the head of a linked list, remove the nth node from the end of the list and return its head.
    
    Example 1:
    
    Input: head = [1,2,3,4,5], n = 2
    Output: [1,2,3,5]
    Example 2:
    
    Input: head = [1], n = 1
    Output: []
    Example 3:
    
    Input: head = [1,2], n = 1
    Output: [1]

---
+++ LeetCode/Assignment19.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class Assignment19
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public static int Length(ListNode head)
+        {
+            ListNode temp = head;
+            int count = 0;
+            while (temp != null)                           
+            {
+                count++;
+                temp = temp.next;
+            }
+            return count;
+        }
+        public static void PrintList(ListNode head)
+        {
+            ListNode ptr = head;
+            while (ptr != null) 
+            {
+                Console.Write(ptr.val + " "); 
+                 ptr = ptr.next; 
+            }
+            Console.WriteLine(); 
+        }
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            int Length = Solution.Length(head); 
+            int nodeFromBeginning= Length - n + 1;
+            ListNode prev = null;
+            ListNode temp = head;
+            for (int i = 1; i < nodeFromBeginning;i++) 
+            {
+                prev = temp;
+                temp = temp.next;
+            }
+            if (prev == null) 
+            {
+                head = head.next; 
+                return head; 
+            }
+            else 
+            {
+                prev.next = prev.next.next; 
+                return head; 
+            }
+        }
+    
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace LeetCode
 {
     internal class Program
     {
+       
         static void Main(string[] args)
         {
 
