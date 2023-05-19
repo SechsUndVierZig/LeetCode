5655a61ad342845d2cf77175a7a5dafff6aa7a89
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-19 12:51:12 +0300

    Assignment21
    You are given the heads of two sorted linked lists list1 and list2.
    
    Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
    
    Return the head of the merged linked list.
    
    Example 1:
    
    Input: list1 = [1,2,4], list2 = [1,3,4]
    Output: [1,1,2,3,4,4]
    Example 2:
    
    Input: list1 = [], list2 = []
    Output: []
    Example 3:
    
    Input: list1 = [], list2 = [0]
    Output: [0]

---
+++ LeetCode/Assignment21.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class Assignment21
+{
+    public class Solution
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode merged = null;
+            ListNode tail = merged;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val <= list2.val)
+                {
+                    ListNode newNode = new ListNode(list1.val);
+                    if (merged == null)
+                        merged = newNode;
+                    else
+                        tail.next = newNode;
+                    tail = newNode;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    ListNode newNode = new ListNode(list2.val);
+                    if (merged == null)
+                        merged = newNode;
+                    else
+                        tail.next = newNode;
+                    tail = newNode;
+                    list2 = list2.next;
+                }
+            }
+
+            if (list1 != null)
+            {
+                if (merged == null)
+                    merged = list1;
+                else
+                    tail.next = list1;
+            }
+
+            if (list2 != null)
+            {
+                if (merged == null)
+                    merged = list2;
+                else
+                    tail.next = list2;
+            }
+
+            tail = null;
+            list1 = null;
+            list2 = null;
+
+            return merged;
+        }
+    }
+}
