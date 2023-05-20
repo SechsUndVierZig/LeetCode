02f2942ed0bc85f4e56df45983b6e1bc4f5499c4
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-20 14:27:31 +0300

    Assignment25
    Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
    
    k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
    
    You may not alter the values in the list's nodes, only nodes themselves may be changed.
    
    Example 1:
    
    Input: head = [1,2,3,4,5], k = 2
    Output: [2,1,4,3,5]
    Example 2:
    
    Input: head = [1,2,3,4,5], k = 3
    Output: [3,2,1,4,5]

---
+++ LeetCode/Assignment25.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Assignment25
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public ListNode ReverseKGroup(ListNode head, int k)
+        {
+            if (k == 1) return head;
+            var fakehead = new ListNode(-1) { next = head };
+            var start = fakehead;
+            var nodeToReverse = head;
+            while (true)
+            {
+                if (canmove(start, k) == null) break;
+                var count = 1;
+                while (count++ < k)
+                {
+                    var tmp = start.next;
+                    start.next = nodeToReverse.next;
+                    nodeToReverse.next = nodeToReverse.next.next;
+                    start.next.next = tmp;
+                }
+                start = nodeToReverse;
+                nodeToReverse = nodeToReverse.next;
+            }
+            return fakehead.next;
+        }
+
+        private ListNode canmove(ListNode head, int k)
+        {
+            var count = 0;
+            while (count++ < k)
+            {
+                if (head == null) return null;
+                head = head.next;
+            }
+            return head;
+        }
+    }
+}
