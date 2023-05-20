861b80d5300867f73069798a722147c6132467a6
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-20 15:33:46 +0300

    Assignment26
    Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
    
    Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
    
    Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
    Return k.
    Custom Judge:
    
    The judge will test your solution with the following code:
    
    int[] nums = [...]; // Input array
    int[] expectedNums = [...]; // The expected answer with correct length
    
    int k = removeDuplicates(nums); // Calls your implementation
    
    assert k == expectedNums.length;
    for (int i = 0; i < k; i++) {
        assert nums[i] == expectedNums[i];
    }
    If all assertions pass, then your solution will be accepted.

---
+++ LeetCode/Assignment26.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Assignment26
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int n = nums.Length;
+
+            if (n == 0 || n == 1)
+                return n;
+
+            int[] temp = new int[n];
+
+            int j = 0;
+            for (int i = 0; i < n - 1; i++)
+                if (nums[i] != nums[i + 1])
+                    temp[j++] = nums[i];
+            temp[j++] = nums[n - 1];
+
+            for (int i = 0; i < j; i++)
+                nums[i] = temp[i];
+
+            return j;
+        }
+    }
+}
