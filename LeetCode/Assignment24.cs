3fa87fa6d8095089e99e93408270739d040b6502
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-20 13:44:15 +0300

    Assignment24
    Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
    
    Example 1:
    
    Input: head = [1,2,3,4]
    Output: [2,1,4,3]
    Example 2:
    
    Input: head = []
    Output: []
    Example 3:
    
    Input: head = [1]
    Output: [1]

---
+++ LeetCode/Assignment24.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Assignment24
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+
+            var newhead = head.next;
+            head.next = SwapPairs(head.next.next);
+            newhead.next = head;
+            return newhead;
+        }
+    }
+}
