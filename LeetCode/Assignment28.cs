9085ddaf967a309c8dbad45010c083b0206b647b
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-21 19:27:00 +0300

    Assignment28
    Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
    
    Example 1:
    
    Input: haystack = "sadbutsad", needle = "sad"
    Output: 0
    Explanation: "sad" occurs at index 0 and 6.
    The first occurrence is at index 0, so we return 0.
    Example 2:
    
    Input: haystack = "leetcode", needle = "leeto"
    Output: -1
    Explanation: "leeto" did not occur in "leetcode", so we return -1.

---
+++ LeetCode/Assignment28.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Assignment28
+{
+    public class Solution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            int index = haystack.IndexOf(needle);
+            return index;
+        }
+    }
+}
