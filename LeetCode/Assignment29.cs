4e2be48fb176e09b953c825db6aaa0731f80513f
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-21 19:27:35 +0300

    Assignment29
    Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
    
    The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
    
    Return the quotient after dividing dividend by divisor.
    
    Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
    
    Example 1:
    
    Input: dividend = 10, divisor = 3
    Output: 3
    Explanation: 10/3 = 3.33333.. which is truncated to 3.
    Example 2:
    
    Input: dividend = 7, divisor = -3
    Output: -2
    Explanation: 7/-3 = -2.33333.. which is truncated to -2.

---
+++ LeetCode/Assignment29.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Assignment29
+{
+    public class Solution
+    {
+        public int Divide(int dividend, int divisor)
+        {
+            if (divisor == 0) return Int32.MaxValue;
+            int sum = 0;
+            try
+            {
+                sum = dividend / divisor;
+                return sum;
+            }
+            catch (OverflowException)
+            {
+                if (sum < Int32.MinValue) return Int32.MinValue;
+                else return Int32.MaxValue;
+            }
+        }
+    }
+}
