890a5f69ba3e97485bf037664462b79a8001063f
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-22 13:06:10 +0300

    Assignment30
    You are given a string s and an array of strings words. All the strings of words are of the same length.
    
    A concatenated substring in s is a substring that contains all the strings of any permutation of words concatenated.
    
    For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are all concatenated strings. "acdbef" is not a concatenated substring because it is not the concatenation of any permutation of words.
    Return the starting indices of all the concatenated substrings in s. You can return the answer in any order.
    
    Example 1:
    
    Input: s = "barfoothefoobarman", words = ["foo","bar"]
    Output: [0,9]
    Explanation: Since words.length == 2 and words[i].length == 3, the concatenated substring has to be of length 6.
    The substring starting at 0 is "barfoo". It is the concatenation of ["bar","foo"] which is a permutation of words.
    The substring starting at 9 is "foobar". It is the concatenation of ["foo","bar"] which is a permutation of words.
    The output order does not matter. Returning [9,0] is fine too.
    Example 2:
    
    Input: s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
    Output: []
    Explanation: Since words.length == 4 and words[i].length == 4, the concatenated substring has to be of length 16.
    There is no substring of length 16 is s that is equal to the concatenation of any permutation of words.
    We return an empty array.
    Example 3:
    
    Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
    Output: [6,9,12]
    Explanation: Since words.length == 3 and words[i].length == 3, the concatenated substring has to be of length 9.
    The substring starting at 6 is "foobarthe". It is the concatenation of ["foo","bar","the"] which is a permutation of words.
    The substring starting at 9 is "barthefoo". It is the concatenation of ["bar","the","foo"] which is a permutation of words.
    The substring starting at 12 is "thefoobar". It is the concatenation of ["the","foo","bar"] which is a permutation of words.

---
+++ LeetCode/Assignment30.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Assignment30
+{
+    public class Solution
+    {
+        public IList<int> FindSubstring(string s, string[] words)
+        {
+
+            int size_word = words[0].Length;
+            int word_count = words.Count();
+
+            int size_L = size_word * word_count;
+
+            List<int> res = new List<int>();
+            if (size_L > s.Length)
+                return res;
+            Dictionary<string, int> hash_map = new Dictionary<string, int>();
+            foreach (string word in words)
+                hash_map[word] = hash_map.GetValueOrDefault(word, 0) + 1;
+            for (int i = 0; i <= s.Length - size_L; i++)
+            {
+                Dictionary<string, int> temp_hash_map = new Dictionary<string, int>(hash_map);
+                int j = i, count = word_count;
+                while (j < i + size_L)
+                {
+                    string word = s.Substring(j, size_word);
+
+                    if (!hash_map.ContainsKey(word) || temp_hash_map[word] == 0)
+                        break;
+                    else
+                    {
+                        temp_hash_map[word]--;
+                        count--;
+                    }
+                    j += size_word;
+                }
+                if (count == 0)
+                    res.Add(i);
+            }
+            return res;
+        }
+    }
+}
