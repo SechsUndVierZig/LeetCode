a28d86b9ab7b0e96ac63d4afb5ed18d61798153e
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-22 14:40:56 +0300

    Assignment31
    A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
    
    For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
    The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
    
    For example, the next permutation of arr = [1,2,3] is [1,3,2].
    Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
    While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
    Given an array of integers nums, find the next permutation of nums.
    
    The replacement must be in place and use only constant extra memory.
    
    Example 1:
    
    Input: nums = [1,2,3]
    Output: [1,3,2]
    Example 2:
    
    Input: nums = [3,2,1]
    Output: [1,2,3]
    Example 3:
    
    Input: nums = [1,1,5]
    Output: [1,5,1]

---
+++ LeetCode/Assignment31.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Assignment31
+{
+    public class Solution
+    {
+        public void NextPermutation(int[] nums)
+        {
+            var startIndex = nums.Length - 2;
+            while (startIndex >= 0)
+            {
+                // Find first decreasing element
+                if (nums[startIndex] < nums[startIndex + 1]) break;
+                --startIndex;
+            }
+
+            if (startIndex >= 0)
+            {
+                int endIndex = nums.Length - 1;
+
+                while (endIndex > startIndex)
+                {
+                    if (nums[endIndex] > nums[startIndex]) break;
+                    --endIndex;
+                }
+                Swap(ref nums[startIndex], ref nums[endIndex]);
+            }
+            Reverse(nums, startIndex + 1);
+        }
+
+        private void Reverse(int[] nums, int startIndex)
+        {
+            for (int start = startIndex, end = nums.Length - 1; start < end; ++start, --end)
+            {
+                Swap(ref nums[start], ref nums[end]);
+            }
+        }
+
+        private void Swap(ref int a, ref int b)
+        {
+            var tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+    }
+}
