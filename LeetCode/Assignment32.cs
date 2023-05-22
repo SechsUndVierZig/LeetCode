5cd8642cf95d0b27f17841c9b6313829f97a4dba
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-22 15:05:04 +0300

    Assignment32
    Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses
    substring
    .
    
    Example 1:
    
    Input: s = "(()"
    Output: 2
    Explanation: The longest valid parentheses substring is "()".
    Example 2:
    
    Input: s = ")()())"
    Output: 4
    Explanation: The longest valid parentheses substring is "()()".
    Example 3:
    
    Input: s = ""
    Output: 0

---
+++ LeetCode/Assignment32.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Assignment32
+{
+    public class Solution
+    {
+        public int LongestValidParentheses(string s)
+        {
+            var dp = new int[s.Length + 1];
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                var continueValidParentheses = GetContinueValidParenthese(s, i);
+                dp[i + 1] = Math.Max(dp[i], continueValidParentheses);
+            }
+            return dp[s.Length];
+        }
+        private int GetContinueValidParenthese(string str, int end)
+        {
+            var res = 0;
+            var temp = 0;
+            var rightParenthese = 0;
+            for (int i = end; i >= 0; i--)
+            {
+                if (str[i] == '(')
+                {
+                    if (rightParenthese == 0) return res;
+
+                    temp += 2;
+                    rightParenthese--;
+                    if (rightParenthese == 0) res = temp;
+                }
+                else rightParenthese++;
+            }
+
+            return res;
+        }
+    }
+}
