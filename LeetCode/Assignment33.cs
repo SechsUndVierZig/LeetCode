6451f6ce5add0591c42d6628468d61d9646b7fcd
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-23 13:45:58 +0300

    Assignment33
    There is an integer array nums sorted in ascending order (with distinct values).
    
    Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
    
    Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
    
    You must write an algorithm with O(log n) runtime complexity.
    
    Example 1:
    
    Input: nums = [4,5,6,7,0,1,2], target = 0
    Output: 4
    Example 2:
    
    Input: nums = [4,5,6,7,0,1,2], target = 3
    Output: -1
    Example 3:
    
    Input: nums = [1], target = 0
    Output: -1

---
+++ LeetCode/Assignment33.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Assignment33
+{
+    public class Solution
+    {
+        public int Search(int[] A, int target)
+        {
+            return this.SearchRecursion(A, 0, A.Length - 1, target);
+        }
+
+        private int SearchRecursion(int[] A, int low, int high, int target)
+        {
+            if (low > high) return -1;
+
+            var mid = (high - low) / 2 + low;
+
+            if (A[mid] == target) return mid;
+
+            if (A[mid] >= A[low])
+            {
+                if (target >= A[low] && target < A[mid])
+                    return this.SearchRecursion(A, low, mid - 1, target);
+
+                return this.SearchRecursion(A, mid + 1, high, target);
+            }
+            else
+            {
+                if (target > A[mid] && target <= A[high])
+                    return this.SearchRecursion(A, mid + 1, high, target);
+
+                return this.SearchRecursion(A, low, mid - 1, target);
+            }
+        }
+    }
+}
