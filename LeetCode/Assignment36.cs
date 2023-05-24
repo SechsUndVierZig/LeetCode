9281ceb5e0f573e42a3f1d9370dc0f1a3f744602
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-24 14:24:15 +0300

    Assignment36
    Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
    
    Each row must contain the digits 1-9 without repetition.
    Each column must contain the digits 1-9 without repetition.
    Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
    Note:
    
    A Sudoku board (partially filled) could be valid but is not necessarily solvable.
    Only the filled cells need to be validated according to the mentioned rules.
    
    Example 1:
    
    Input: board =
    [["5","3",".",".","7",".",".",".","."]
    ,["6",".",".","1","9","5",".",".","."]
    ,[".","9","8",".",".",".",".","6","."]
    ,["8",".",".",".","6",".",".",".","3"]
    ,["4",".",".","8",".","3",".",".","1"]
    ,["7",".",".",".","2",".",".",".","6"]
    ,[".","6",".",".",".",".","2","8","."]
    ,[".",".",".","4","1","9",".",".","5"]
    ,[".",".",".",".","8",".",".","7","9"]]
    Output: true
    Example 2:
    
    Input: board =
    [["8","3",".",".","7",".",".",".","."]
    ,["6",".",".","1","9","5",".",".","."]
    ,[".","9","8",".",".",".",".","6","."]
    ,["8",".",".",".","6",".",".",".","3"]
    ,["4",".",".","8",".","3",".",".","1"]
    ,["7",".",".",".","2",".",".",".","6"]
    ,[".","6",".",".",".",".","2","8","."]
    ,[".",".",".","4","1","9",".",".","5"]
    ,[".",".",".",".","8",".",".","7","9"]]
    Output: false
    Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.

---
+++ LeetCode/Assignment36.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment36
+{
+    public class Solution
+    {
+        public bool IsValidSudoku(char[][] board)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (!isParticallyValid(board, i, 0, i, 8))
+                    return false;
+                if (!isParticallyValid(board, 0, i, 8, i))
+                    return false;
+            }
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    if (!isParticallyValid(board, i * 3, j * 3, i * 3 + 2, j * 3 + 2))
+                        return false;
+            return true;
+        }
+
+        bool isParticallyValid(char[][] board, int x1, int y1, int x2, int y2)
+        {
+            var set = new HashSet<char>();
+            for (int row = x1; row <= x2; row++)
+                for (int col = y1; col <= y2; col++)
+                    if (board[row][col] != '.')
+                        if (!set.Add(board[row][col]))
+                            return false;
+            return true;
+        }
+
+    }
+}
