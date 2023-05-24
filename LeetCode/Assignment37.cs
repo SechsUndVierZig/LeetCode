b7e7d91d93eb06fb53224f80b43761917b0f9b99
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-24 14:50:06 +0300

    Assignment37
    Write a program to solve a Sudoku puzzle by filling the empty cells.
    
    A sudoku solution must satisfy all of the following rules:
    
    Each of the digits 1-9 must occur exactly once in each row.
    Each of the digits 1-9 must occur exactly once in each column.
    Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
    The '.' character indicates empty cells.
    
    Example 1:
    
    Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
    Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
    Explanation: The input board is shown above and the only valid solution is shown below:

---
+++ LeetCode/Assignment37.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Assignment37
+{
+    public class Solution
+    {
+        public void SolveSudoku(char[][] board)
+        {
+            if (board == null) return;
+            backtrack(board, 0, 0);
+        }
+        private bool backtrack(char[][] board, int row, int col)
+        {
+            if (col == 9)
+            {
+                col = 0; ++row;
+                if (row == 9) return true;
+            }
+            if (board[row][col] != '.')
+                return backtrack(board, row, col + 1);
+
+            for (char v = '1'; v <= '9'; v++)
+            {
+                if (isValid(board, row, col, v))
+                {
+                    board[row][col] = v;
+                    if (backtrack(board, row, col + 1)) return true;
+                    else board[row][col] = '.';
+                }
+            }
+            return false;
+        }
+        private bool isValid(char[][] board, int row, int col, char val)
+        {
+            for (int r = 0; r < 9; r++)
+                if (board[r][col] == val) return false;
+            for (int c = 0; c < 9; c++)
+                if (board[row][c] == val) return false;
+            int I = row / 3; int J = col / 3;
+            for (int a = 0; a < 3; a++)
+                for (int b = 0; b < 3; b++)
+                    if (val == board[3 * I + a][3 * J + b]) return false;
+            return true;
+
+        }
+    }
+}
