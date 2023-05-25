dcea22480a1236dc93f974250804a1b533f98f1d
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-25 16:54:58 +0300

    Assignment41
    Given an unsorted integer array nums, return the smallest missing positive integer.
    
    You must implement an algorithm that runs in O(n) time and uses constant extra space.
    
    Example 1:
    
    Input: nums = [1,2,0]
    Output: 3
    Explanation: The numbers in the range [1,2] are all in the array.
    Example 2:
    
    Input: nums = [3,4,-1,1]
    Output: 2
    Explanation: 1 is in the array but 2 is missing.
    Example 3:
    
    Input: nums = [7,8,9,11,12]
    Output: 1
    Explanation: The smallest positive integer 1 is missing.

---
+++ LeetCode/Assignment41.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment41
+{
+    public class Solution
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>(nums);
+            int i = 1;
+            while (true)
+            {
+                if (!set.Contains(i))
+                    return i;
+                i++;
+            }
+            return -1;
+        }
+    }
