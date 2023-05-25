e39e428f694e8ee0166a11c87e0627deb6a9bdf5
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-25 16:22:10 +0300

    Assignment39
    Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
    
    The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the
    frequency
     of at least one of the chosen numbers is different.
    
    The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
    
    Example 1:
    
    Input: candidates = [2,3,6,7], target = 7
    Output: [[2,2,3],[7]]
    Explanation:
    2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
    7 is a candidate, and 7 = 7.
    These are the only two combinations.
    Example 2:
    
    Input: candidates = [2,3,5], target = 8
    Output: [[2,2,2,2],[2,3,3],[3,5]]
    Example 3:
    
    Input: candidates = [2], target = 1
    Output: []

---
+++ LeetCode/Assignment39.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Assignment39
+{
+    public class Solution
+    {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            Array.Sort(candidates);
+            IList<IList<int>> res = new List<IList<int>>();
+            Find(res, candidates, target, 0, new List<int>());
+            return res;
+        }
+
+        public void Find(IList<IList<int>> lists, int[] arr, int target, int start, IList<int> curr)
+        {
+            if (target == 0)
+            {
+                if (!lists.Contains(curr))
+                    lists.Add(new List<int>(curr));
+                return;
+            }
+
+            for (int i = start; i < arr.Length && arr[i] <= target; i++)
+            {
+                curr.Add(arr[i]);
+                Find(lists, arr, target - arr[i], i, curr);
+                curr.RemoveAt(curr.Count - 1);
+            }
+        }
+    }
+}
