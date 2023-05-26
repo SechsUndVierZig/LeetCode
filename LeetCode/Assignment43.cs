4f2fa4430bdaa8c5ce28f9d5c896b784c64f3353
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-26 18:09:22 +0300

    Assignment43
    Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
    
    Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
    
    Example 1:
    
    Input: num1 = "2", num2 = "3"
    Output: "6"
    Example 2:
    
    Input: num1 = "123", num2 = "456"
    Output: "56088"

---
+++ LeetCode/Assignment43.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Numerics;
+
+public class Assignment43
+{
+    public class Solution
+    {
+        public string Multiply(string num1, string num2)
+        {
+            BigInteger n = BigInteger.Parse(num1);
+            BigInteger n1 = BigInteger.Parse(num2);
+            BigInteger k = n * n1;
+            return k.ToString();
+        }
+    }
+}
