48218d69e7e122d06afef0e5eea869dea4ede7d5
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-26 17:59:05 +0300

    Assignment42
    Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
    
    Example 1:
    
    Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
    Output: 6
    Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
    Example 2:
    
    Input: height = [4,2,0,3,2,5]
    Output: 9

---
+++ LeetCode/Assignment42.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Assignment42
+{
+    public class Solution
+    {
+        public int Trap(int[] height)
+        {
+            var res = 0;
+            var max = 0;
+            var left = 0;
+            var right = height.Length - 1;
+
+            while (left <= right)
+            {
+                if (height[left] < height[right])
+                {
+                    max = Math.Max(max, height[left]);
+                    res += max - height[left];
+                    left++;
+                }
+                else
+                {
+                    max = Math.Max(max, height[right]);
+                    res += max - height[right];
+                    right--;
+                }
+            }
+
+            return res;
+        }
+    }
+}
