945661b6de6fdee99d7f78b9078f72b31ead6218
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-27 13:18:51 +0300

    Assignment45
    You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
    
    Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
    
    0 <= j <= nums[i] and
    i + j < n
    Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
    
    Example 1:
    
    Input: nums = [2,3,1,1,4]
    Output: 2
    Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
    Example 2:
    
    Input: nums = [2,3,0,1,4]
    Output: 2

---
+++ LeetCode/Assignment45.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Assignment45
+{
+    public class Solution
+    {
+        public int Jump(int[] nums)
+        {
+            int[] res = new int[nums.Length];
+
+            for (int i = 1; i < res.Length; i++)
+                res[i] = JumpAux(nums, res, i);
+            return res[res.Length - 1];
+        }
+
+
+        public int JumpAux(int[] nums, int[] res, int index)
+        {
+            if (index == 0)
+                return 0;
+
+            if (res[index] > 0)
+                return res[index];
+            int minJump = int.MaxValue;
+            for (int i = 0; i < index; i++)
+                if (nums[i] + i >= index)
+                    if (res[i] + 1 < minJump)
+                        minJump = res[i] + 1;
+            return minJump;
+        }
+
+    }
+}
