cbe5af15cb33b462d7b5ea027b8608f325c15473
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-27 13:59:42 +0300

    Assignment46
    Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
    
    Example 1:
    
    Input: nums = [1,2,3]
    Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
    Example 2:
    
    Input: nums = [0,1]
    Output: [[0,1],[1,0]]
    Example 3:
    
    Input: nums = [1]
    Output: [[1]]

---
+++ LeetCode/Assignment46.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment46
+{
+    public class Solution
+    {
+        public IList<IList<int>> Permute(int[] nums)
+        {
+            IList<int> tempLst = new List<int>();
+            IList<IList<int>> result = new List<IList<int>>();
+
+            PerformPermutations(nums, result, tempLst);
+            return result;
+        }
+
+        private void PerformPermutations(int[] nums, IList<IList<int>> result, IList<int> tempLst)
+        {
+            if (tempLst.Count == nums.Length)
+            {
+                result.Add(new List<int>(tempLst));
+                return;
+            }
+            else
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (tempLst.Contains(nums[i]))
+                        continue;
+                    tempLst.Add(nums[i]);
+                    PerformPermutations(nums, result, tempLst);
+                    tempLst.RemoveAt(tempLst.Count - 1);
+                }
+        }
+    }
+}
