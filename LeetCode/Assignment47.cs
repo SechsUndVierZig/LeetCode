02b6609916cce795121d7be428a6c0af021ae9c8
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-27 15:44:43 +0300

    Assignment47
    Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
    
    Example 1:
    
    Input: nums = [1,1,2]
    Output:
    [[1,1,2],
     [1,2,1],
     [2,1,1]]
    Example 2:
    
    Input: nums = [1,2,3]
    Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]

---
+++ LeetCode/Assignment47.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment47
+{
+    public class Solution
+    {
+        public IList<IList<int>> PermuteUnique(int[] nums)
+        {
+            List<IList<int>> res = new List<IList<int>>();
+            Array.Sort(nums);
+            bool[] used = new bool[nums.Length];
+            Backtracking(nums, new List<int>(), res, used);
+            return res;
+        }
+
+        private void Backtracking(int[] nums, List<int> list, List<IList<int>> res, bool[] used)
+        {
+            if (list.Count == nums.Length)
+            {
+                res.Add(new List<int>(list));
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (i > 0 && nums[i] == nums[i - 1] && used[i - 1] || used[i]) continue;
+
+                    list.Add(nums[i]);
+                    used[i] = true;
+                    Backtracking(nums, list, res, used);
+                    list.RemoveAt(list.Count - 1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
