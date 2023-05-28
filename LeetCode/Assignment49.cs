b8453c631b4c2afc0cc848ef0864fea86ebfb6f0
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-28 13:25:37 +0300

    Assignment49
    Given an array of strings strs, group the anagrams together. You can return the answer in any order.
    
    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
    
    Example 1:
    
    Input: strs = ["eat","tea","tan","ate","nat","bat"]
    Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
    Example 2:
    
    Input: strs = [""]
    Output: [[""]]
    Example 3:
    
    Input: strs = ["a"]
    Output: [["a"]]

---
+++ LeetCode/Assignment49.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment49
+{
+    public class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var dict = new Dictionary<string, IList<string>>();
+            for (int i = 0; i < strs.Length; i++)
+            {
+                char[] arr = strs[i].ToCharArray();
+                Array.Sort(arr);
+                String sorted = new String(arr);
+
+                if (!dict.ContainsKey(sorted)) dict.Add(sorted, new List<String>() { strs[i] });
+                else dict[sorted].Add(strs[i]);
+
+            }
+            return dict.Values.ToList();
+        }
+    }
+}
