bbbc502404805bb82c81a368c4be145f06e7169c
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-28 12:57:10 +0300

    Assignment48
    You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
    
    You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
    
    Example 1:
    
    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
    Output: [[7,4,1],[8,5,2],[9,6,3]]
    Example 2:
    
    Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
    Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]

---
+++ LeetCode/Assignment48.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Assignment48
+{
+    public class Solution
+    {
+        public int[][] Rotate(int[][] matrix)
+        {
+            int n = matrix.Length;
+            for (int layer = 0; layer < n / 2; layer++)
+            {
+                int first = layer;
+                int last = n - 1 - layer;
+                for (int i = first; i < last; i++)
+                {
+                    int offset = i - first;
+
+                    //top
+                    int top = matrix[first][i];
+
+                    //top replaced by left
+                    matrix[first][i] = matrix[last - offset][first];
+
+                    //left replaced by bottom
+                    matrix[last - offset][first] = matrix[last][last - offset];
+
+                    //bottom replaced by right
+                    matrix[last][last - offset] = matrix[i][last];
+
+                    //right replaced by top
+                    matrix[i][last] = top;
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
