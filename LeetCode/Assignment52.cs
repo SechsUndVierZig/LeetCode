8994a5edcc0a41262dd0ac13dbbaf074d889fd1e
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-29 15:07:12 +0300

    Assignment52
    The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
    
    Given an integer n, return the number of distinct solutions to the n-queens puzzle.
    
    Example 1:
    
    Input: n = 4
    Output: 2
    Explanation: There are two distinct solutions to the 4-queens puzzle as shown.
    Example 2:
    
    Input: n = 1
    Output: 1

---
+++ LeetCode/Assignment52.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Assignment52
+{
+    public class Solution
+    {
+        public int TotalNQueens(int n)
+        {
+            return this.TotalNQueens(n, 0, new int[n], new bool[n]);
+        }
+        public int TotalNQueens(int n, int y, int[] prevX, bool[] usedX)
+        {
+            // when we run into y, [0, y) row queens are already in place
+            if (n == y) return 1;
+            int retval = 0;
+
+            // Go through all valid X
+            for (int x = 0; x < n; x++)
+            {
+                bool valid = !usedX[x];
+                for (int prevY = 0; prevY < y && valid; prevY++)
+                {
+                    if (Math.Abs(prevY - y) == Math.Abs(prevX[prevY] - x)) valid = false; // if cross happens, it's invalid
+                }
+
+                if (valid)
+                {
+                    prevX[y] = x;
+                    usedX[x] = true;
+                    retval += this.TotalNQueens(n, y + 1, prevX, usedX);
+                    usedX[x] = false;
+                }
+            }
+
+            return retval;
+        }
+    }
+}
