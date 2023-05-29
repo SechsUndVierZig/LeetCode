1856e13d77554dc3065ab5047d99aec947dc6662
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-29 12:58:05 +0300

    Assignment51
    The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
    
    Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
    
    Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
    
    Example 1:
    
    Input: n = 4
    Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
    Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
    Example 2:
    
    Input: n = 1
    Output: [["Q"]]

---
+++ LeetCode/Assignment51.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Assignment51
+{
+    public class Solution
+    {
+        public IList<IList<string>> SolveNQueens(int n)
+        {
+            if (n == 1) return new List<IList<string>> { new List<string> { "Q" } };
+
+            var result = new List<IList<string>>();
+            var board = new List<IList<string>>();
+            var queensList = new List<(int i, int j)>();
+
+            for (int i = 0; i < n; i++)
+                board.Add(Enumerable.Repeat(".", n).ToList());
+            Solve(board, n, 0, queensList, result);
+            return result;
+        }
+
+        private void Solve(List<IList<string>> board, int n, int row, List<(int i, int j)> queensList, List<IList<string>> result)
+        {
+            if (queensList.Count == n)
+            {
+                var current = board.Select(row => string.Join("", row)).ToList();
+                result.Add(current);
+                return;
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                if (!IsValidPosition(queensList, row, j)) continue;
+                queensList.Add((row, j));
+                board[row][j] = "Q";
+                Solve(board, n, row + 1, queensList, result);
+                queensList.RemoveAt(queensList.Count - 1);
+                board[row][j] = ".";
+            }
+        }
+
+        private bool IsValidPosition(List<(int i, int j)> queensList, int i, int j)
+        {
+            foreach (var queen in queensList)
+            {
+                if (queen.i == i) return false;
+                if (queen.j == j) return false;
+                if (Math.Abs(queen.i - i) == Math.Abs(queen.j - j)) return false;
+            }
+            return true;
+        }
+    }
+}
