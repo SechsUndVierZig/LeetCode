a2329d6f6b9ea6f8f0284b54380043f9f47abd84
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-29 12:28:21 +0300

    Assignment50
    Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
    
    Example 1:
    
    Input: x = 2.00000, n = 10
    Output: 1024.00000
    Example 2:
    
    Input: x = 2.10000, n = 3
    Output: 9.26100
    Example 3:
    
    Input: x = 2.00000, n = -2
    Output: 0.25000
    Explanation: 2-2 = 1/22 = 1/4 = 0.25

---
+++ LeetCode/Assignment50.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Assignment50
+{
+    public class Solution
+    {
+        public double MyPow(double x, int n)
+        {
+            long N = n;
+            if (N < 0)
+            {
+                x = 1 / x;
+                N = -N;
+            }
+            double ans = 1;
+            double curr = x;
+            for (long i = N; i > 0; i = i / 2)
+            {
+                if (i % 2 == 1) ans = ans * curr;
+                curr = curr * curr;
+            }
+
+            return ans;
+        }
+    }
+}
