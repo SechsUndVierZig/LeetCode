57d5da8be3c515a7eb572379222aedf74448d3b2
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-30 13:23:26 +0300

    Assignment54
    Given an m x n matrix, return all elements of the matrix in spiral order.
    
    Example 1:
    
    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
    Output: [1,2,3,6,9,8,7,4,5]
    Example 2:
    
    Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
    Output: [1,2,3,4,8,12,11,10,9,5,6,7]

---
+++ LeetCode/Assignment54.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment54
+{
+    public class Solution
+    {
+        public IList<int> SpiralOrder(int[][] matrix)
+        {
+            List<int> res = new List<int>();
+            if (matrix == null || matrix.Length == 0)
+                return res;
+
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+            int top = 0, bottom = m - 1, left = 0, right = n - 1;
+
+            while (top <= bottom && left <= right)
+            {
+                for (int j = left; j <= right && top <= bottom && left <= right; j++)
+                    res.Add(matrix[top][j]);
+                top++;
+
+                for (int i = top; i <= bottom && top <= bottom && left <= right; i++)
+                    res.Add(matrix[i][right]);
+                right--;
+
+                for (int j = right; j >= left && top <= bottom && left <= right; j--)
+                    res.Add(matrix[bottom][j]);
+                bottom--;
+
+                for (int i = bottom; i >= top && top <= bottom && left <= right; i--)
+                    res.Add(matrix[i][left]);
+                left++;
+            }
+
+            return res;
+        }
+    }
+}
