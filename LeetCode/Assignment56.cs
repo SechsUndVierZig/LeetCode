46cf15c264300e43e05d63a11afb00407fae61bb
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-31 14:56:16 +0300

    Assignment56
    Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
    
    Example 1:
    
    Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
    Output: [[1,6],[8,10],[15,18]]
    Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
    Example 2:
    
    Input: intervals = [[1,4],[4,5]]
    Output: [[1,5]]
    Explanation: Intervals [1,4] and [4,5] are considered overlapping.

---
+++ LeetCode/Assignment56.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Assignment56
+{
+    public class Solution
+    {
+        public int[][] Merge(int[][] intervals)
+        {
+            List<int[]> results = intervals.ToList();
+
+            if (intervals.Length <= 1) return intervals;
+
+            int[] start = new int[intervals.Length];
+            int[] end = new int[intervals.Length];
+
+            for (int k = 0; k < intervals.Length; k++)
+            {
+                start[k] = results[k][0];
+                end[k] = results[k][1];
+            }
+
+            Array.Sort(start);
+            Array.Sort(end);
+
+            results.Clear();
+
+            int[] current = new int[] { start[0], end[0] };
+            int i = 0;
+            while (i < intervals.Length)
+            {
+                if (current[1] >= start[i])
+                    current[1] = end[i];
+
+                else
+                {
+                    results.Add(current);
+                    current = new int[] { start[i], end[i] };
+                }
+                i++;
+            }
+            results.Add(current);
+
+            return results.ToArray();
+        }
+    }
+}
