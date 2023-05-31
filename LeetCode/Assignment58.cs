67b02865d19727981bc03054cc07fa07cba3b1c7
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-05-31 14:57:10 +0300

    Assignment58
    Given a string s consisting of words and spaces, return the length of the last word in the string.
    
    A word is a maximal
    substring
     consisting of non-space characters only.
    
    Example 1:
    
    Input: s = "Hello World"
    Output: 5
    Explanation: The last word is "World" with length 5.
    Example 2:
    
    Input: s = "   fly me   to   the moon  "
    Output: 4
    Explanation: The last word is "moon" with length 4.
    Example 3:
    
    Input: s = "luffy is still joyboy"
    Output: 6
    Explanation: The last word is "joyboy" with length 6.

---
+++ LeetCode/Assignment58.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Assignment58
+{
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            var endIndex = s.Length - 1;
+            while (endIndex >= 0 && s[endIndex] == ' ')
+                endIndex--;
+            var startIndex = endIndex;
+            while (startIndex >= 0 && s[startIndex] != ' ')
+                startIndex--;
+            return endIndex - startIndex;
+        }
+    }
+}
