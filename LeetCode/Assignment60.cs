c5d57f4fdad401355cfce8ae5d3b8fd89d506c83
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-01 14:09:40 +0300

    Assignment60
    he set [1, 2, 3, ..., n] contains a total of n! unique permutations.
    
    By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
    
    "123"
    "132"
    "213"
    "231"
    "312"
    "321"
    Given n and k, return the kth permutation sequence.

---
+++ LeetCode/Assignment60.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment60
+{
+    public class Solution
+    {
+        private int factorial(int n)
+        {
+            if (n == 1) return 1;
+            else return n * factorial(n - 1);
+        }
+        private string backtracking(int N, int K, List<int> set, string permutation)
+        {
+            if (N == 0) return permutation;
+
+            int fact = factorial(N),
+                combinations = fact / N,
+                index = 0;
+
+            int[] windows = new int[N];
+            for (int i = 0, c = combinations; i < N; i++, c += combinations)
+            {
+                windows[i] = c;
+                index += (K > c) ? 1 : 0;
+            }
+
+            permutation += set[index];
+            set.RemoveAt(index);
+            K -= (index > 0) ? windows[index - 1] : index;
+            return backtracking(N - 1, K, set, permutation);
+        }
+
+        public string GetPermutation(int n, int k)
+        {
+            List<int> set = new List<int>();
+            for (int i = 1; i <= n; i++) set.Add(i);
+            return backtracking(n, k, set, "");
+        }
+    }
+}
