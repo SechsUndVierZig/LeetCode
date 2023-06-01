e2372c372493ec93236641f448de3baa91f4c005
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-01 14:11:27 +0300

    Assignment61
    Given the head of a linked list, rotate the list to the right by k places.
    
    Example 1:
    
    Input: head = [1,2,3,4,5], k = 2
    Output: [4,5,1,2,3]
    Example 2:
    
    Input: head = [0,1,2], k = 4
    Output: [2,0,1]

---
+++ LeetCode/Assignment61.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Assignment61
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            if (head == null || head.next == null || k == 0) return head;
+            ListNode last = head;
+            int length = 1;
+            while (last.next != null)
+            {
+                last = last.next;
+                length++;
+            }
+            k = k % length;
+            if (k == 0) return head;
+
+            ListNode current = head;
+            for (int i = 1; i < length - k; i++) current = current.next;
+
+            ListNode newhead = current.next;
+            current.next = null;
+            last.next = head;
+            return newhead;
+        }
+    }
+}
