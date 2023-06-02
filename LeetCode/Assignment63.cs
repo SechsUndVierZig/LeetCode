e57066cad366cbc4f74c54efc309a84955ba1fe5
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-02 13:10:05 +0300

    Assignment63
    You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
    
    An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
    
    Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
    
    The testcases are generated so that the answer will be less than or equal to 2 * 109.
    
    Example 1:
    
    Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
    Output: 2
    Explanation: There is one obstacle in the middle of the 3x3 grid above.
    There are two ways to reach the bottom-right corner:
    1. Right -> Right -> Down -> Down
    2. Down -> Down -> Right -> Right
    Example 2:
    
    Input: obstacleGrid = [[0,1],[0,0]]
    Output: 1

---
+++ LeetCode/Assignment63.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Assignment63
+{
+    public class Solution
+    {
+        public int UniquePathsWithObstacles(int[][] obstacleGrid)
+        {
+            for (int i = 0; i < obstacleGrid.Length; i++)
+            {
+                for (int j = 0; j < obstacleGrid[0].Length; j++)
+                {
+                    obstacleGrid[i][j] = (obstacleGrid[i][j], i, j) switch
+                    {
+                        (1, _, _) => 0,
+                        (_, 0, 0) => 1,
+                        (_, 0, _) => obstacleGrid[i][j - 1] * 1,
+                        (_, _, 0) => obstacleGrid[i - 1][j] * 1,
+                        _ => obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
+                    };
+                }
+            }
+            return obstacleGrid[^1][^1];
+        }
+    }
+}
