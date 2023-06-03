ce692872147260f7f02658de6826aea5b1f3b0ba
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-03 13:36:01 +0300

    Assignment66
    You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
    
    Increment the large integer by one and return the resulting array of digits.
    
    Example 1:
    
    Input: digits = [1,2,3]
    Output: [1,2,4]
    Explanation: The array represents the integer 123.
    Incrementing by one gives 123 + 1 = 124.
    Thus, the result should be [1,2,4].
    Example 2:
    
    Input: digits = [4,3,2,1]
    Output: [4,3,2,2]
    Explanation: The array represents the integer 4321.
    Incrementing by one gives 4321 + 1 = 4322.
    Thus, the result should be [4,3,2,2].
    Example 3:
    
    Input: digits = [9]
    Output: [1,0]
    Explanation: The array represents the integer 9.
    Incrementing by one gives 9 + 1 = 10.
    Thus, the result should be [1,0].

---
+++ LeetCode/Assignment66.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Assignment66
+{
+    public class Solution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            int n = digits.Length;
+            int carry = 1;
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int sum = digits[i] + carry;
+                digits[i] = sum % 10;
+                carry = sum / 10;
+
+                if (carry == 0) break;
+
+            }
+
+            if (carry == 1)
+            {
+                int[] result = new int[n + 1];
+                result[0] = 1;
+                digits.CopyTo(result, 1);
+                return result;
+            }
+
+            return digits;
+        }
+    }
+}
