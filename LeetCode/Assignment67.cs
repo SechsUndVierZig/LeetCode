d9cb99b994211dddee262577cb82d8b57174e30b
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-04 13:05:23 +0300

    Assignment67
    Given two binary strings a and b, return their sum as a binary string.
    
    Example 1:
    
    Input: a = "11", b = "1"
    Output: "100"
    Example 2:
    
    Input: a = "1010", b = "1011"
    Output: "10101"

---
+++ LeetCode/Assignment67.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+public class Assignment67
+{
+    public class Solution
+    {
+        public string AddBinary(string a, string b)
+        {
+            var resultBuilder = new StringBuilder();
+            int i = a.Length - 1, j = b.Length - 1;
+            int carriage = 0;
+            for (; i >= 0 || j >= 0; i--, j--)
+            {
+                carriage += (i >= 0 ? a[i] - 48 : 0);     // 48 -> '0'
+                carriage += (j >= 0 ? b[j] - 48 : 0);    // 48 -> '0'
+                resultBuilder.Insert(0, carriage % 2);
+                carriage = carriage / 2;
+            }
+            if (carriage > 0)
+                resultBuilder.Insert(0, carriage);
+            return resultBuilder.ToString();
+        }
+    }
+}
