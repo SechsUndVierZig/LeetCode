3dc76f40dec704ef2870b43b6a03ca72f0023234
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-06 12:20:25 +0300

    Assignment72
    Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
    
    You have the following three operations permitted on a word:
    
    Insert a character
    Delete a character
    Replace a character
    
    Example 1:
    
    Input: word1 = "horse", word2 = "ros"
    Output: 3
    Explanation:
    horse -> rorse (replace 'h' with 'r')
    rorse -> rose (remove 'r')
    rose -> ros (remove 'e')
    Example 2:
    
    Input: word1 = "intention", word2 = "execution"
    Output: 5
    Explanation:
    intention -> inention (remove 't')
    inention -> enention (replace 'i' with 'e')
    enention -> exention (replace 'n' with 'x')
    exention -> exection (replace 'n' with 'c')
    exection -> execution (insert 'u')

---
+++ LeetCode/Assignment72.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+public class Assignment72
+{
+    public class Solution
+    {
+        public int MinDistance(string word1, string word2)
+        {
+            var m = word1.Length;
+            var n = word2.Length;
+            var prevRow = new int[n + 1];
+
+            for (var j = 0; j <= n; j++)
+                prevRow[j] = j;
+
+            for (var i = 0; i < m; i++)
+            {
+                var curRow = new int[n + 1];
+                curRow[0] = i + 1;
+
+                for (var j = 0; j < n; j++)
+                {
+                    if (word1[i] == word2[j])
+                        curRow[j + 1] = Min(prevRow[j], prevRow[j + 1] + 1, curRow[j] + 1);
+                    else
+                        curRow[j + 1] = Min(prevRow[j], prevRow[j + 1], curRow[j]) + 1;
+                }
+                prevRow = curRow;
+            }
+
+            // return dp[m, n];
+            return prevRow[n];
+
+            int Min(params int[] arr)
+            {
+                return arr.Min();
+            }
+        }
+    }
+}
