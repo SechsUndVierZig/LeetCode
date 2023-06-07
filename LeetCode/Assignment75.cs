cd0f2f9dfc2fead9f67bb7e70b8529297a66d4b1
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-07 13:56:40 +0300

    Assignment75
    Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
    
    We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
    
    You must solve this problem without using the library's sort function.
    
    Example 1:
    
    Input: nums = [2,0,2,1,1,0]
    Output: [0,0,1,1,2,2]
    Example 2:
    
    Input: nums = [2,0,1]
    Output: [0,1,2]

---
+++ LeetCode/Assignment75.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Assignment75
+{
+    public class Solution
+    {
+        public void SortColors(int[] nums)
+        {
+            int r, w, b;
+            r = w = b = 0;
+
+            foreach (var i in nums)
+            {
+                if (i == 0) r++;
+                else if (i == 1) w++;
+                else b++;
+            }
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (r > 0)
+                {
+                    nums[i] = 0;
+                    r--;
+                }
+                else if (w > 0)
+                {
+                    nums[i] = 1;
+                    w--;
+                }
+                else
+                    nums[i] = 2;
+            }
+        }
+    }
+}
