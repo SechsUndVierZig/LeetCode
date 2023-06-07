7c47bf9b72f5d002bf4423a1576b0dd8b66b32ff
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-07 13:20:15 +0300

    Assignment74
    You are given an m x n integer matrix matrix with the following two properties:
    
    Each row is sorted in non-decreasing order.
    The first integer of each row is greater than the last integer of the previous row.
    Given an integer target, return true if target is in matrix or false otherwise.
    
    You must write a solution in O(log(m * n)) time complexity.
    
    Example 1:
    
    Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
    Output: true
    Example 2:
    
    Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
    Output: false

---
+++ LeetCode/Assignment74.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Assignment74
+{
+    public class Solution
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            var (rows, cols) = (matrix.Length, matrix[0].Length);
+            var (l, r) = (0, rows * cols - 1);
+            while (l < r)
+            {
+                var m = l + (r - l) / 2;
+
+                (l, r) = matrix[m / cols][m % cols] >= target ? (l, m) : (m + 1, r);
+            }
+            return matrix[l / cols][l % cols] == target;
+        }
+    }
+}
