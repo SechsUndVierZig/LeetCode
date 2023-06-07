5011d839ea6c90b185c4ccbcf67534fef8a9d6cf
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-07 15:20:14 +0300

    Assignment76
    Given two strings s and t of lengths m and n respectively, return the minimum window
    substring
     of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
    
    The testcases will be generated such that the answer is unique.
    
    Example 1:
    
    Input: s = "ADOBECODEBANC", t = "ABC"
    Output: "BANC"
    Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
    Example 2:
    
    Input: s = "a", t = "a"
    Output: "a"
    Explanation: The entire string s is the minimum window.
    Example 3:
    
    Input: s = "a", t = "aa"
    Output: ""
    Explanation: Both 'a's from t must be included in the window.
    Since the largest window of s only has one 'a', return empty string.

---
+++ LeetCode/Assignment76.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Assignment76
+{
+    public class Solution
+    {
+        public string MinWindow(string s, string t)
+        {
+            var map = new int[128, 2];
+            foreach (var t1 in t)
+                map[t1, 0]++;
+            var left = 0;
+            var matches = 0;
+            var result = string.Empty;
+            for (int right = 0; right < s.Length; right++)
+            {
+                map[s[right], 1]++;
+
+                if (map[s[right], 0] >= map[s[right], 1])
+                    matches++;
+
+                while (matches == t.Length)
+                {
+                    if (right - left + 1 < result.Length || result.Length == 0)
+                        result = s.Substring(left, right - left + 1);
+
+                    map[s[left], 1]--;
+                    if (map[s[left], 0] > map[s[left], 1])
+                        matches--;
+
+                    left++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
