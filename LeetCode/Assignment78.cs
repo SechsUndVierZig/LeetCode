d609c8b19d958f9a3c6b46444e89e9498e9e40f1
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-08 14:39:16 +0300

    Assignment78
    Given an integer array nums of unique elements, return all possible
    subsets
     (the power set).
    
    The solution set must not contain duplicate subsets. Return the solution in any order.
    
    Example 1:
    
    Input: nums = [1,2,3]
    Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
    Example 2:
    
    Input: nums = [0]
    Output: [[],[0]]

---
+++ LeetCode/Assignment78.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment78
+{
+    public class Solution
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            IList<IList<int>> subsets = new List<IList<int>>();
+            subsets.Add(new List<int>());
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int count = subsets.Count;
+                for (int j = 0; j < count; j++)
+                {
+                    var temp = subsets[j].ToList();
+                    temp.Add(nums[i]);
+                    subsets.Add(temp);
+                }
+            }
+            return subsets;
+        }
+    }
+}
