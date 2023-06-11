1689838827abde4d19f1481bd8917d6a4634a422
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-11 14:44:13 +0300

    Assignment83
    Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
    
    Example 1:
    
    Input: head = [1,1,2]
    Output: [1,2]
    Example 2:
    
    Input: head = [1,1,2,3,3]
    Output: [1,2,3]

---
+++ LeetCode/Assignment83.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Assignment83
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            ListNode n = head;
+            while (n is not null)
+            {
+                while (n.val == n.next?.val)
+                    n.next = n.next.next;
+                n = n.next;
+            }
+            return head;
+        }
+    }
+}
