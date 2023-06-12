5fb9fe6fbe286b0bc8e725abf07ab02489530091
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-12 12:48:26 +0300

    Assignment84
    Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
    
    Example 1:
    
    Input: heights = [2,1,5,6,2,3]
    Output: 10
    Explanation: The above is a histogram where width of each bar is 1.
    The largest rectangle is shown in the red area, which has an area = 10 units.
    Example 2:
    
    Input: heights = [2,4]
    Output: 4

---
+++ LeetCode/Assignment84.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment84
+{
+    public class Solution
+    {
+        public int LargestRectangleArea(int[] heights)
+        {
+            int n = heights.Length;
+            int[] left = new int[n];
+            int[] right = new int[n];
+            Stack<int> s = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                while (s.Count > 0 && heights[s.Peek()] >= heights[i])
+                    s.Pop();
+                left[i] = s.Count == 0 ? 0 : s.Peek() + 1;
+                s.Push(i);
+            }
+
+            s.Clear();
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                while (s.Count > 0 && heights[s.Peek()] >= heights[i])
+                    s.Pop();
+                right[i] = s.Count == 0 ? n - 1 : s.Peek() - 1;
+                s.Push(i);
+            }
+            int maxArea = 0;
+            for (int i = 0; i < n; i++)
+                maxArea = Math.Max(maxArea, heights[i] * (right[i] - left[i] + 1));
+            return maxArea;
+        }
+    }
+}
