7af2ddcddc3ff822eb41ca307e81455bc7fbdb1e
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-13 13:31:35 +0300

    Assignment86
    Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
    
    You should preserve the original relative order of the nodes in each of the two partitions.
    
    Example 1:
    
    Input: head = [1,4,3,2,5,2], x = 3
    Output: [1,2,2,4,3,5]
    Example 2:
    
    Input: head = [2,1], x = 2
    Output: [1,2]

---
+++ LeetCode/Assignment86.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment86
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public ListNode Partition(ListNode head, int x)
+        {
+            List<ListNode> list = new List<ListNode>();
+            while (head != null)
+            {
+                list.Add(head);
+                head = head.next;
+            }
+            if (list.Count == 0) return null;
+            else if (list.Count == 1) return list[0];
+            ListNode[] nodes = new ListNode[list.Count];
+            int index = 0;
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                if (list[i].val >= x) continue;
+                nodes[index] = list[i];
+                index++;
+            }
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                if (list[i].val < x) continue;
+                nodes[index] = list[i];
+                index++;
+            }
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                nodes[i].next = (i + 1 < nodes.Length) ? nodes[i + 1] : null;
+            }
+            return nodes[0];
+        }
+    }
+}
