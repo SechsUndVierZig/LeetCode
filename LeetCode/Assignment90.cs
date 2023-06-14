83cc107a9ff08ffd78eb63ecf8507b5d86bd6958
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-14 20:19:06 +0300

    Assignment90
    Given an integer array nums that may contain duplicates, return all possible
    subsets
     (the power set).
    
    The solution set must not contain duplicate subsets. Return the solution in any order.
    
    Example 1:
    
    Input: nums = [1,2,2]
    Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
    Example 2:
    
    Input: nums = [0]
    Output: [[],[0]]

---
+++ LeetCode/Assignment90.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Assignment90
+{
+    public class Solution
+    {
+        public IList<IList<int>> SubsetsWithDup(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return null;
+            IList<IList<int>> result = new List<IList<int>>();
+            Array.Sort(nums);
+            Backtrack(result, new List<int>(), 0, nums);
+            return result;
+        }
+        private void Backtrack(IList<IList<int>> sets, List<int> curr, int index, int[] nums)
+        {
+            sets.Add(new List<int>(curr));
+            for (int i = index; i < nums.Length; i++)
+            {
+                if (i > index && nums[i] == nums[i - 1]) continue;
+                curr.Add(nums[i]);
+                Backtrack(sets, curr, i + 1, nums);
+                curr.RemoveAt(curr.Count - 1);
+            }
+        }
+    }
+}
