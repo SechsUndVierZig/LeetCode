8b3ffc62e891c15997bb1b12f397e4159d3d800a
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-14 13:46:46 +0300

    Assignment89
    An n-bit gray code sequence is a sequence of 2n integers where:
    
    Every integer is in the inclusive range [0, 2n - 1],
    The first integer is 0,
    An integer appears no more than once in the sequence,
    The binary representation of every pair of adjacent integers differs by exactly one bit, and
    The binary representation of the first and last integers differs by exactly one bit.
    Given an integer n, return any valid n-bit gray code sequence.
    
    Example 1:
    
    Input: n = 2
    Output: [0,1,3,2]
    Explanation:
    The binary representation of [0,1,3,2] is [00,01,11,10].
    - 00 and 01 differ by one bit
    - 01 and 11 differ by one bit
    - 11 and 10 differ by one bit
    - 10 and 00 differ by one bit
    [0,2,3,1] is also a valid gray code sequence, whose binary representation is [00,10,11,01].
    - 00 and 10 differ by one bit
    - 10 and 11 differ by one bit
    - 11 and 01 differ by one bit
    - 01 and 00 differ by one bit
    Example 2:
    
    Input: n = 1
    Output: [0,1]

---
+++ LeetCode/Assignment89.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment89
+{
+    public class Solution
+    {
+        public IList<int> GrayCode(int n)
+        {
+            if (n == 1)
+                return new List<int> { 0, 1 };
+
+            IList<int> list = GrayCode(n - 1);
+            IList<int> ans = new List<int>();
+
+            for (int i = 0; i < list.Count; i++)
+                ans.Add(0 << n - 1 | list[i]);
+
+            for (int i = list.Count() - 1; i >= 0; i--)
+                ans.Add(1 << n - 1 | list[i]);
+
+            return ans;
+        }
+    }
+}
