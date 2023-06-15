6d0bf753c1a057398523e5e059012a9b65cfe5c1
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-15 12:36:02 +0300

    Assignment93
    A valid IP address consists of exactly four integers separated by single dots. Each integer is between 0 and 255 (inclusive) and cannot have leading zeros.
    
    For example, "0.1.2.201" and "192.168.1.1" are valid IP addresses, but "0.011.255.245", "192.168.1.312" and "192.168@1.1" are invalid IP addresses.
    Given a string s containing only digits, return all possible valid IP addresses that can be formed by inserting dots into s. You are not allowed to reorder or remove any digits in s. You may return the valid IP addresses in any order.
    
    Example 1:
    
    Input: s = "25525511135"
    Output: ["255.255.11.135","255.255.111.35"]
    Example 2:
    
    Input: s = "0000"
    Output: ["0.0.0.0"]
    Example 3:
    
    Input: s = "101023"
    Output: ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]

---
+++ LeetCode/Assignment93.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment93
+{
+    public class Solution
+    {
+        public IList<string> RestoreIpAddresses(string s)
+        {
+            List<string> r = new List<string>();
+            if (s.Length > 12 || s.Length < 4) return r;
+
+
+            void solve(int i, int dots, string cur)
+            {
+                if (dots == 4 && i == s.Length)
+                {
+                    r.Add(cur.Remove(cur.Length - 1, 1));
+                    return;
+                }
+
+                for (int j = i; j < Math.Min(i + 3, s.Length); j++)
+                {
+                    string part = s.Substring(i, j - i + 1);
+                    if (part[0] == '0' && part.Length > 1) continue;
+                    if (Int32.Parse(part) < 256)
+                    {
+                        solve(j + 1, dots + 1, cur + part + ".");
+                    }
+
+                }
+            }
+
+
+            solve(0, 0, "");
+            return r;
+
+        }
+    }
+}
