b1b52b0827f91effc8ec63af39042655e7e95f1d
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-15 11:58:38 +0300

    Assignment92
    Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
    
    Example 1:
    
    Input: head = [1,2,3,4,5], left = 2, right = 4
    Output: [1,4,3,2,5]
    Example 2:
    
    Input: head = [5], left = 1, right = 1
    Output: [5]

---
+++ LeetCode/Assignment92.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class Assignment92
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public ListNode ReverseBetween(ListNode head, int left, int right)
+        {
+            if (left == right)
+                return head;
+
+            ListNode prev = null;
+            ListNode pres = head;
+            ListNode next = head.next;
+            int count = 1;
+            while (count < left)
+            {
+                prev = pres;
+                pres = next;
+                next = next.next;
+                count++;
+            }
+
+            ListNode newEnd = pres;
+            ListNode last = prev;
+            while (count < right)
+            {
+                ListNode p = next.next;
+                pres.next = prev;
+                next.next = pres;
+                prev = pres;
+                pres = next;
+                next = p;
+                count++;
+            }
+
+            newEnd.next = next;
+
+            if (last == null) return pres;
+
+            last.next = pres;
+            return head;
+        }
+    }
+}
