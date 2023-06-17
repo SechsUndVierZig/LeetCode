b36c21294d68dbcb0802725f795c7c0c9a11f405
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-17 14:26:56 +0300

    Assignment97
    Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
    
    An interleaving of two strings s and t is a configuration where s and t are divided into n and m
    substrings
     respectively, such that:
    
    s = s1 + s2 + ... + sn
    t = t1 + t2 + ... + tm
    |n - m| <= 1
    The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
    Note: a + b is the concatenation of strings a and b.
    
    Example 1:
    
    Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
    Output: true
    Explanation: One way to obtain s3 is:
    Split s1 into s1 = "aa" + "bc" + "c", and s2 into s2 = "dbbc" + "a".
    Interleaving the two splits, we get "aa" + "dbbc" + "bc" + "a" + "c" = "aadbbcbcac".
    Since s3 can be obtained by interleaving s1 and s2, we return true.
    Example 2:
    
    Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
    Output: false
    Explanation: Notice how it is impossible to interleave s2 with any other string to obtain s3.
    Example 3:
    
    Input: s1 = "", s2 = "", s3 = ""
    Output: true

---
+++ LeetCode/Assignment97.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Assignment97
+{
+    public class Solution
+    {
+        public bool IsInterleave(string s1, string s2, string s3)
+        {
+            if (s1.Length + s2.Length != s3.Length)
+                return false;
+
+            var cache = new int[s1.Length + 1][];
+
+            for (var i = 0; i < cache.Length; i++)
+                cache[i] = new int[s2.Length + 1];
+
+            return IsInterleave(0, 0);
+
+            bool IsInterleave(int p1, int p2)
+            {
+                int p3 = p1 + p2;
+
+                if (p3 == s3.Length) return true;
+
+                if (cache[p1][p2] != 0) return cache[p1][p2] == 1;
+
+                var r = p1 < s1.Length && s1[p1] == s3[p3] && IsInterleave(p1 + 1, p2)
+                     || p2 < s2.Length && s2[p2] == s3[p3] && IsInterleave(p1, p2 + 1);
+
+                cache[p1][p2] = r ? 1 : -1;
+
+                return r;
+            }
+        }
+    }
+}
