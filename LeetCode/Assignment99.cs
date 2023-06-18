9ad6388594d64b8c50c4d2dcad00d6cc039b8fc9
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-18 14:50:41 +0300

    Assignment99
    You are given the root of a binary search tree (BST), where the values of exactly two nodes of the tree were swapped by mistake. Recover the tree without changing its structure.
    
    Example 1:
    
    Input: root = [1,3,null,null,2]
    Output: [3,1,null,null,2]
    Explanation: 3 cannot be a left child of 1 because 3 > 1. Swapping 1 and 3 makes the BST valid.
    Example 2:
    
    Input: root = [3,1,4,null,null,2]
    Output: [2,1,4,null,null,3]
    Explanation: 2 cannot be in the right subtree of 3 because 2 < 3. Swapping 2 and 3 makes the BST valid.

---
+++ LeetCode/Assignment99.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Assignment99
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public void RecoverTree(TreeNode root)
+        {
+            TreeNode prev = null; // prev node on treverse
+
+            TreeNode c1 = null; // candidates to swap
+            TreeNode c2 = null;
+
+            Traverse(root);
+
+            (c1.val, c2.val) = (c2.val, c1.val);
+
+            return;
+
+            void Traverse(TreeNode n)
+            {
+                if (n == null) return;
+
+                Traverse(n.left);
+
+                if (prev != null && prev.val > n.val)
+                {
+                    c1 = c1 ?? prev;
+                    c2 = n;
+                }
+
+                prev = n;
+
+                Traverse(n.right);
+            }
+        }
+    }
+}
