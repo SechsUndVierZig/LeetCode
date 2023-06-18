a68bbc2970100fe502a03ae38eeaae0f8a762ed3
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-18 14:39:33 +0300

    Assignment98
    Given the root of a binary tree, determine if it is a valid binary search tree (BST).
    
    A valid BST is defined as follows:
    
    The left
    subtree
     of a node contains only nodes with keys less than the node's key.
    The right subtree of a node contains only nodes with keys greater than the node's key.
    Both the left and right subtrees must also be binary search trees.
    
    Example 1:
    
    Input: root = [2,1,3]
    Output: true
    Example 2:
    
    Input: root = [5,1,4,null,null,3,6]
    Output: false
    Explanation: The root node's value is 5 but its right child's value is 4.

---
+++ LeetCode/Assignment98.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment98
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            int? l = null;
+            Stack<TreeNode> s = new Stack<TreeNode>();
+            TreeNode cur = root;
+            while (cur != null || s.Count() > 0)
+            {
+                while (cur != null)
+                {
+                    s.Push(cur);
+                    cur = cur.left;
+                }
+                cur = s.Pop();
+                if (l != null && l >= cur.val) return false;
+                l = cur.val;
+                cur = cur.right;
+            }
+
+            return true;
+        }
+    }
+}
