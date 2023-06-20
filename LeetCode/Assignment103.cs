9764191a40ae9d8a7db3c2776446326b3dfe4850
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-20 11:20:34 +0300

    Assignment103
    Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
    
    Example 1:
    
    Input: root = [3,9,20,null,null,15,7]
    Output: [[3],[20,9],[15,7]]
    Example 2:
    
    Input: root = [1]
    Output: [[1]]
    Example 3:
    
    Input: root = []
    Output: []

---
+++ LeetCode/Assignment103.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment103
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            List<IList<int>> ret = new();
+            if (root == null) return ret;
+            ZigzagLevelOrderSub(root, ret, 0);
+            return ret;
+        }
+        private void ZigzagLevelOrderSub(TreeNode nd, List<IList<int>> ret, int level)
+        {
+            if (ret.Count < level + 1) ret.Add(new List<int>());
+            if (level % 2 == 0)
+                ret[level].Add(nd.val);
+            else
+                ret[level].Insert(0, nd.val);
+
+            if (nd.left != null) ZigzagLevelOrderSub(nd.left, ret, level + 1);
+            if (nd.right != null) ZigzagLevelOrderSub(nd.right, ret, level + 1);
+        }
+    }
+}
