f2ae97295e07c040ea4435903a3444eee811298d
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-22 14:34:47 +0300

    Assignment107
    Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
    
    Example 1:
    
    Input: root = [3,9,20,null,null,15,7]
    Output: [[15,7],[9,20],[3]]
    Example 2:
    
    Input: root = [1]
    Output: [[1]]
    Example 3:
    
    Input: root = []
    Output: []

---
+++ LeetCode/Assignment107.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment107
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            IList<IList<int>> res = new List<IList<int>>();
+
+            if (root == null) return res;
+
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(root);
+
+            while (q.Count > 0)
+            {
+                var qCount = q.Count;
+                IList<int> currentLevel = new List<int>();
+
+                while (qCount-- > 0)
+                {
+                    var currentNode = q.Dequeue();
+                    currentLevel.Add(currentNode.val);
+
+                    if (currentNode.left != null) q.Enqueue(currentNode.left);
+
+
+                    if (currentNode.right != null) q.Enqueue(currentNode.right);
+
+                }
+
+                res.Insert(0, currentLevel);
+            }
+
+            return res;
+        }
+    }
+}
