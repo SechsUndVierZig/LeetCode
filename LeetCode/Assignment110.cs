d44ddc4dabb8b87697fd2744e14711c6619b6f01
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-23 14:14:49 +0300

    Assignment110
    Given a binary tree, determine if it is
    height-balanced
    .
    
    Example 1:
    
    Input: root = [3,9,20,null,null,15,7]
    Output: true
    Example 2:
    
    Input: root = [1,2,2,3,3,null,null,4,4]
    Output: false
    Example 3:
    
    Input: root = []
    Output: true

---
+++ LeetCode/Assignment110.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Assignment110
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        private bool res = true;
+        public bool IsBalanced(TreeNode root)
+        {
+
+            if (root == null)
+                return true;
+            DFS(root, 0);
+
+            return res;
+        }
+
+        private int DFS(TreeNode node, int h)
+        {
+
+            if (node == null)
+                return 0;
+
+            int lh = DFS(node.left, h + 1),
+            rh = DFS(node.right, h + 1);
+
+            if (Math.Abs(lh - rh) > 1)
+                res = false;
+
+            return Math.Max(lh, rh) + 1;
+        }
+    }
+}
