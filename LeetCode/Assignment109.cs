a8c1872c82f1f12ffed06244ed19f3b4d52b876e
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-23 13:49:54 +0300

    Assignment109
    Given the head of a singly linked list where elements are sorted in ascending order, convert it to a
    height-balanced
     binary search tree.
    
    Example 1:
    
    Input: head = [-10,-3,0,5,9]
    Output: [0,-3,9,-10,null,5]
    Explanation: One possible answer is [0,-3,9,-10,null,5], which represents the shown height balanced BST.
    Example 2:
    
    Input: head = []
    Output: []

---
+++ LeetCode/Assignment109.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment109
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    public class Solution
+    {
+        public TreeNode SortedListToBST(ListNode head)
+        {
+            List<int> values = new List<int>();
+            while (head != null)
+            {
+                values.Add(head.val);
+                head = head.next;
+            }
+            return CreateSortedListToBSTNode(values, 0, values.Count - 1);
+        }
+
+        TreeNode CreateSortedListToBSTNode(List<int> values, int start, int end)
+        {
+            if (start < 0 || end >= values.Count || start > end)
+                return null;
+
+            int mid = (start + end) / 2;
+            return new TreeNode(values[mid], CreateSortedListToBSTNode(values, start, mid - 1), CreateSortedListToBSTNode(values, mid + 1, end));
+        }
+    }
+}
