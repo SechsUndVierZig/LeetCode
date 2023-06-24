90f89299386efab5c6f98983b59d14dcc48633d0
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-24 13:16:16 +0300

    Assignment113
    Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
    
    A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
    
    Example 1:
    
    Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
    Output: [[5,4,11,2],[5,8,4,5]]
    Explanation: There are two paths whose sum equals targetSum:
    5 + 4 + 11 + 2 = 22
    5 + 8 + 4 + 5 = 22
    Example 2:
    
    Input: root = [1,2,3], targetSum = 5
    Output: []
    Example 3:
    
    Input: root = [1,2], targetSum = 0
    Output: []

---
+++ LeetCode/Assignment113.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment113
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public IList<IList<int>> PathSum(TreeNode root, int targetSum)
+        {
+            var res = new List<IList<int>>();
+            var sub = new List<int>();
+
+            if (root == null)
+                return res;
+
+
+            return FindPaths(root, targetSum, res, sub);
+        }
+
+        public IList<IList<int>> FindPaths(TreeNode root, int targetSum, List<IList<int>> res, List<int> sub)
+        {
+            sub.Add(root.val);
+            if (root.right == null && root.left == null && targetSum == root.val)
+            {
+                res.Add(new List<int>(sub));
+                return res;
+            }
+            if (root.left != null)
+            {
+                FindPaths(root.left, targetSum - root.val, res, sub);
+                sub.RemoveAt(sub.Count - 1);
+            }
+            if (root.right != null)
+            {
+                FindPaths(root.right, targetSum - root.val, res, sub);
+                sub.RemoveAt(sub.Count - 1);
+            }
+            return res;
+        }
+    }
+}
