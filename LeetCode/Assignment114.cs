0d40b5d79a217b049a5a1ff61d0949bd8c1c5c4c
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-25 11:59:11 +0300

    Assignment114
    Given the root of a binary tree, flatten the tree into a "linked list":
    
    The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
    The "linked list" should be in the same order as a pre-order traversal of the binary tree.
    
    Example 1:
    
    Input: root = [1,2,5,3,4,null,6]
    Output: [1,null,2,null,3,null,4,null,5,null,6]
    Example 2:
    
    Input: root = []
    Output: []
    Example 3:
    
    Input: root = [0]
    Output: [0]

---
+++ LeetCode/Assignment114.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class Assignment114
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public void Flatten(TreeNode root)
+        {
+            if (root == null)
+                return;
+
+            flatten(root);
+        }
+
+        private TreeNode flatten(TreeNode root)
+        {
+            if (root.left == null && root.right == null)
+                return root;
+
+            if (root.left == null || root.right == null)
+            {
+                if (root.right == null)
+                    moveLeftToRight(root);
+
+                return flatten(root.right);
+            }
+
+            TreeNode lastLeft = flatten(root.left);
+            TreeNode lastRight = flatten(root.right);
+            TreeNode tempRight = root.right;
+
+            moveLeftToRight(root);
+            lastLeft.right = tempRight;
+
+            return lastRight;
+        }
+
+        private void moveLeftToRight(TreeNode root)
+        {
+            root.right = root.left;
+            root.left = null;
+        }
+    }
+}
