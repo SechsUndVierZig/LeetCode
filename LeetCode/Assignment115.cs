78bc3419217d0cb69232a14258e73ecfe00ade6b
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-25 12:18:56 +0300

    Assignment115
    Given two strings s and t, return the number of distinct
    subsequences
     of s which equals t.
    
    The test cases are generated so that the answer fits on a 32-bit signed integer.
    
    Example 1:
    
    Input: s = "rabbbit", t = "rabbit"
    Output: 3
    Explanation:
    As shown below, there are 3 ways you can generate "rabbit" from s.
    rabbbit
    rabbbit
    rabbbit
    Example 2:
    
    Input: s = "babgbag", t = "bag"
    Output: 5
    Explanation:
    As shown below, there are 5 ways you can generate "bag" from s.
    babgbag
    babgbag
    babgbag
    babgbag
    babgbag

---
+++ LeetCode/Assignment115.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Assignment115
+{
+    public class Solution
+    {
+        public int NumDistinct(string s, string t)
+        {
+            int?[,] dp = new int?[s.Length, t.Length];
+            return Backtracking(0, 0);
+
+            int Backtracking(int indexOfInput, int indexOfTarget)
+            {
+                if (indexOfTarget == t.Length) return 1;
+                if (indexOfInput == s.Length) return 0;
+
+                if (dp[indexOfInput, indexOfTarget] is { } result) return result;
+
+                if (s[indexOfInput] != t[indexOfTarget]) return dp[indexOfInput, indexOfTarget] ??= Backtracking(indexOfInput + 1, indexOfTarget);
+
+                return dp[indexOfInput, indexOfTarget] ??= Backtracking(indexOfInput + 1, indexOfTarget)
+                                                           + Backtracking(indexOfInput + 1, indexOfTarget + 1);
+            }
+        }
+    }
+}
