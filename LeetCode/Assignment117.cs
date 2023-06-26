e2521fadebae4be9885244ecc12fa0fe8edb692e
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-26 13:27:42 +0300

    Assignment117
    Given a binary tree
    
    struct Node {
      int val;
      Node *left;
      Node *right;
      Node *next;
    }
    Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
    
    Initially, all next pointers are set to NULL.
    
    Example 1:
    
    Input: root = [1,2,3,4,5,null,7]
    Output: [1,#,2,3,#,4,5,7,#]
    Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
    Example 2:
    
    Input: root = []
    Output: []

---
+++ LeetCode/Assignment117.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment117
+{
+    /*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
+*/
+
+    public class Solution
+    {
+        public Node Connect(Node root)
+        {
+            if (root == null) return root;
+            Queue<Node> q = new Queue<Node>();
+            q.Enqueue(root);
+            while (q.Count > 0)
+            {
+                int count = q.Count;
+                Node prev = null;
+                for (int i = 0; i < count; i++)
+                {
+                    Node cur = q.Dequeue();
+                    if (cur.right != null) q.Enqueue(cur.right);
+                    if (cur.left != null) q.Enqueue(cur.left);
+                    cur.next = prev;
+                    prev = cur;
+                }
+            }
+            return root;
+        }
+    }
+}
