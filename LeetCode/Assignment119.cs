0ef87879ecc05973c886ef18dd668949c8709ad2
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-27 14:27:33 +0300

    Assignment119
    Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
    
    In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
    
    Example 1:
    
    Input: rowIndex = 3
    Output: [1,3,3,1]
    Example 2:
    
    Input: rowIndex = 0
    Output: [1]
    Example 3:
    
    Input: rowIndex = 1
    Output: [1,1]

---
+++ LeetCode/Assignment119.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment119
+{
+    public class Solution
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            int[] v = new int[rowIndex + 1];
+            v[0] = 1;
+            for (int i = 0; i <= rowIndex; ++i)
+            {
+                for (int j = i; j > 0; --j)
+                    v[j] = v[j] + v[j - 1];
+            }
+            return v;
+        }
+    }
+}
