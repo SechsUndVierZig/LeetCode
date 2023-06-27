f43fe49130cd62317d9ef35caed5a4e6529bab0f
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-27 14:23:17 +0300

    Assignment118
    Given an integer numRows, return the first numRows of Pascal's triangle.
    
    In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
    
    Example 1:
    
    Input: numRows = 5
    Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
    Example 2:
    
    Input: numRows = 1
    Output: [[1]]

---
+++ LeetCode/Assignment118.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Assignment118
+{
+    public class Solution
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            var rs = new List<IList<int>> { new List<int> { 1 } };
+            for (int i = 2; i <= numRows; i++)
+            {
+                var list = new List<int> { 1 };
+                for (int j = 1; j < i - 1; j++)
+                    list.Add(rs[rs.Count - 1][j - 1] + rs[rs.Count - 1][j]);
+
+                list.Add(1);
+                rs.Add(list);
+            }
+            return rs;
+        }
+    }
+}
