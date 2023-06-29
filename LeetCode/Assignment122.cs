a279b69e4d34a1be531fd786d8f711042c6ce5dd
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-29 20:48:04 +0300

    Assignment122
    You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
    
    On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
    
    Find and return the maximum profit you can achieve.
    
    Example 1:
    
    Input: prices = [7,1,5,3,6,4]
    Output: 7
    Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
    Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
    Total profit is 4 + 3 = 7.
    Example 2:
    
    Input: prices = [1,2,3,4,5]
    Output: 4
    Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
    Total profit is 4.
    Example 3:
    
    Input: prices = [7,6,4,3,1]
    Output: 0
    Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.

---
+++ LeetCode/Assignment122.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Assignment122
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Length < 2) { return 0; }
+            int min = prices[0];
+            int max = prices[0];
+            int sum = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > max)
+                {
+                    max = prices[i];
+                    continue;
+                }
+                if (prices[i] < max)
+                {
+                    sum += max - min;
+                    min = prices[i];
+                    max = prices[i];
+                }
+            }
+            sum += max - min;
+
+            return sum;
+        }
+    }
+}
