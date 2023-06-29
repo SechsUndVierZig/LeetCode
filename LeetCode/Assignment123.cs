571d1f1b402350608d400a06ac0e7a29acbec1cb
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-29 20:49:27 +0300

    Assignment123
    You are given an array prices where prices[i] is the price of a given stock on the ith day.
    
    Find the maximum profit you can achieve. You may complete at most two transactions.
    
    Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
    
    Example 1:
    
    Input: prices = [3,3,5,0,0,3,1,4]
    Output: 6
    Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
    Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
    Example 2:
    
    Input: prices = [1,2,3,4,5]
    Output: 4
    Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
    Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging multiple transactions at the same time. You must sell before buying again.
    Example 3:
    
    Input: prices = [7,6,4,3,1]
    Output: 0
    Explanation: In this case, no transaction is done, i.e. max profit = 0.

---
+++ LeetCode/Assignment123.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment123
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            var endpointsOfBestSequences = new List<(int, int)>();
+
+            var currentBuyIndex = 0;
+
+            for (var i = 1; i <= prices.Length; i++)
+            {
+                var currentPriceOrEndGuard = i < prices.Length
+                    ? prices[i]
+                    : -1;
+
+                var haveReachedEndOfIncreasingSequence = currentPriceOrEndGuard < prices[i - 1];
+
+                if (!haveReachedEndOfIncreasingSequence)
+                {
+                    continue;
+                }
+
+                var currentSequence = (currentBuyIndex, i - 1);
+
+                var haveNotYetSelectedTwoSequences = endpointsOfBestSequences.Count < 2;
+
+                if (haveNotYetSelectedTwoSequences)
+                {
+                    endpointsOfBestSequences.Add(currentSequence);
+
+                    currentBuyIndex = i;
+
+                    continue;
+                }
+
+                var appliedOption = ChooseAndApplyBestOptionForCombiningSequences(
+                    endpointsOfBestSequences,
+                    currentSequence,
+                    prices
+                );
+
+                var shouldStartNewSequenceAfterCurrentOneGotUsed =
+                    appliedOption != SequenceCombinationOption.KeepFirstAndSecondExisting;
+
+                if (shouldStartNewSequenceAfterCurrentOneGotUsed)
+                {
+                    currentBuyIndex = i;
+
+                    continue;
+                }
+
+                var shouldDumpCurrentBuyPriceForCurrentPrice = currentPriceOrEndGuard < prices[currentBuyIndex];
+
+                if (shouldDumpCurrentBuyPriceForCurrentPrice)
+                {
+                    currentBuyIndex = i;
+                }
+            }
+
+            return SumUpBestSequences(endpointsOfBestSequences, prices);
+        }
+
+        private SequenceCombinationOption ChooseAndApplyBestOptionForCombiningSequences(
+            List<(int, int)> endpointsOfBestSequences,
+            (int, int) currentSequence,
+            int[] prices)
+        {
+            var optionsForCombiningSequences = GetOptionsForCombiningSequences(
+                endpointsOfBestSequences,
+                currentSequence,
+                prices
+            );
+
+            Array.Sort(optionsForCombiningSequences, (option1, option2) => option2.Item2 - option1.Item2);
+
+            var bestCombinationOption = optionsForCombiningSequences[0];
+
+            ApplySequenceCombinationOption(
+                bestCombinationOption,
+                endpointsOfBestSequences,
+                currentSequence);
+
+            return bestCombinationOption.Item1;
+        }
+
+        private enum SequenceCombinationOption
+        {
+            KeepFirstAndSecondExisting,
+            MergeExistingAndAlsoTakeCurrent,
+            KeepFirstAndCurrent,
+            KeepSecondAndCurrent,
+            KeepFirstAndMergeSecondWithCurrent,
+        }
+
+        private (SequenceCombinationOption, int)[] GetOptionsForCombiningSequences(
+            List<(int, int)> endpointsOfBestSequences,
+            (int, int) currentSequence,
+            int[] prices)
+        {
+            var firstExistingSequence = endpointsOfBestSequences[0];
+            var secondExistingSequence = endpointsOfBestSequences[1];
+
+            var firstExistingSequenceProfit = prices[firstExistingSequence.Item2] - prices[firstExistingSequence.Item1];
+            var secondExistingSequenceProfit = prices[secondExistingSequence.Item2] - prices[secondExistingSequence.Item1];
+
+            var currentSequenceProfit = prices[currentSequence.Item2] - prices[currentSequence.Item1];
+
+            return new (SequenceCombinationOption, int)[] {
+            (SequenceCombinationOption.KeepFirstAndSecondExisting, firstExistingSequenceProfit + secondExistingSequenceProfit),
+            (SequenceCombinationOption.MergeExistingAndAlsoTakeCurrent, prices[secondExistingSequence.Item2] - prices[firstExistingSequence.Item1] + currentSequenceProfit),
+            (SequenceCombinationOption.KeepFirstAndCurrent, firstExistingSequenceProfit + currentSequenceProfit),
+            (SequenceCombinationOption.KeepSecondAndCurrent, secondExistingSequenceProfit + currentSequenceProfit),
+            (SequenceCombinationOption.KeepFirstAndMergeSecondWithCurrent,firstExistingSequenceProfit + prices[currentSequence.Item2] - prices[secondExistingSequence.Item1]),
+        };
+        }
+
+        private void ApplySequenceCombinationOption(
+            (SequenceCombinationOption, int) combinationOption,
+            List<(int, int)> endpointsOfBestSequences,
+            (int, int) currentSequence)
+        {
+            switch (combinationOption.Item1)
+            {
+                case SequenceCombinationOption.KeepFirstAndSecondExisting:
+                    break;
+                case SequenceCombinationOption.MergeExistingAndAlsoTakeCurrent:
+                    MergeExistingSequencesAndTakeCurrent(endpointsOfBestSequences, currentSequence);
+                    break;
+                case SequenceCombinationOption.KeepFirstAndCurrent:
+                    endpointsOfBestSequences[1] = currentSequence;
+                    break;
+                case SequenceCombinationOption.KeepSecondAndCurrent:
+                    KeepSecondAndCurrentSequences(endpointsOfBestSequences, currentSequence);
+                    break;
+                case SequenceCombinationOption.KeepFirstAndMergeSecondWithCurrent:
+                    endpointsOfBestSequences[1] = (endpointsOfBestSequences[1].Item1, currentSequence.Item2);
+                    break;
+            }
+        }
+
+        private void MergeExistingSequencesAndTakeCurrent(
+            List<(int, int)> endpointsOfBestSequences,
+            (int, int) currentSequence)
+        {
+            var firstExistingSequence = endpointsOfBestSequences[0];
+            var secondExistingSequence = endpointsOfBestSequences[1];
+
+            endpointsOfBestSequences.Clear();
+
+            endpointsOfBestSequences.Add((firstExistingSequence.Item1, secondExistingSequence.Item2));
+
+            endpointsOfBestSequences.Add(currentSequence);
+        }
+
+        private void KeepSecondAndCurrentSequences(
+            List<(int, int)> endpointsOfBestSequences,
+            (int, int) currentSequence)
+        {
+            var secondExistingSequence = endpointsOfBestSequences[1];
+
+            endpointsOfBestSequences.Clear();
+
+            endpointsOfBestSequences.Add(secondExistingSequence);
+
+            endpointsOfBestSequences.Add(currentSequence);
+        }
+
+        private int SumUpBestSequences(List<(int, int)> endpointsOfBestSequences, int[] prices)
+        {
+            var toReturn = 0;
+
+            foreach (var sequence in endpointsOfBestSequences)
+            {
+                toReturn += prices[sequence.Item2] - prices[sequence.Item1];
+            }
+
+            return toReturn;
+        }
+    }
+}
