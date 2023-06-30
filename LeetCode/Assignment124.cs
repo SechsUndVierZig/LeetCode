c34775317d24cd1516386267a48b11f942f03378
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-06-30 12:13:03 +0300

    Assignment124
    A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
    
    The path sum of a path is the sum of the node's values in the path.
    
    Given the root of a binary tree, return the maximum path sum of any non-empty path.
    
    Example 1:
    
    Input: root = [1,2,3]
    Output: 6
    Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
    Example 2:
    
    Input: root = [-10,9,20,null,null,15,7]
    Output: 42
    Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.

---
+++ LeetCode/Assignment124.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Assignment124
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public int MaxPathSum(TreeNode root)
+        {
+            int maxSum = int.MinValue;
+            Traverse(root, ref maxSum);
+            return maxSum;
+        }
+
+        private int Traverse(TreeNode node, ref int maxSum)
+        {
+            if (node == null)
+                return 0;
+
+            int leftSum = Math.Max(0, Traverse(node.left, ref maxSum));
+            int rightSum = Math.Max(0, Traverse(node.right, ref maxSum));
+
+            int pathSum = leftSum + rightSum + node.val;
+            maxSum = Math.Max(maxSum, pathSum);
+
+            return Math.Max(leftSum, rightSum) + node.val;
+        }
+    }
+}
