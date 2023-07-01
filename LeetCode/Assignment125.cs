15b52fae843ec1de181baa5d8afb5b44dd85ce19
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-01 18:45:35 +0300

    Assignment125
    A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
    
    Given a string s, return true if it is a palindrome, or false otherwise.
    
    Example 1:
    
    Input: s = "A man, a plan, a canal: Panama"
    Output: true
    Explanation: "amanaplanacanalpanama" is a palindrome.
    Example 2:
    
    Input: s = "race a car"
    Output: false
    Explanation: "raceacar" is not a palindrome.
    Example 3:
    
    Input: s = " "
    Output: true
    Explanation: s is an empty string "" after removing non-alphanumeric characters.
    Since an empty string reads the same forward and backward, it is a palindrome.

---
+++ LeetCode/Assignment125.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public class Assignment125
+{
+    public class Solution
+    {
+        public bool IsPalindrome(string s)
+        {
+            var clean = s.ToLower().Where(x => char.IsLetterOrDigit(x));
+            return clean.Reverse().SequenceEqual(clean);
+        }
+    }
+}
