82af1dd3c2bca98fa5a2d07b2e8d290972ff9f82
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-02 14:48:47 +0300

    Assignment127
    A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
    
    Every adjacent pair of words differs by a single letter.
    Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
    sk == endWord
    Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
    
    Example 1:
    
    Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
    Output: 5
    Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
    Example 2:
    
    Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
    Output: 0
    Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.

---
+++ LeetCode/Assignment127.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment127
+{
+    public class Solution
+    {
+        Dictionary<string, int> map;
+        IList<string> wordlist;
+        public int LadderLength(string beginWord, string endWord, IList<string> wordList1)
+        {
+            map = new Dictionary<string, int>();
+            wordlist = wordList1;
+            for (int i = 0; i < wordlist.Count; i++)
+            {
+                map[wordlist[i]] = i;
+            }
+
+            if (!map.ContainsKey(endWord)) return 0;
+
+            int target = map[endWord];
+            int n = wordlist.Count;
+
+            wordlist.Add(beginWord);
+
+            Queue<int> q = new Queue<int>();
+            HashSet<int> uplayerset = new HashSet<int>();
+            q.Enqueue(n);
+
+            uplayerset.Add(n);
+            int depth = 0;
+            bool reached = false;
+
+            while (q.Count > 0 && !reached)
+            {
+                var nextlayerset = new HashSet<int>();
+                depth++;
+
+                var qsize = q.Count;
+                for (int i = 0; i < qsize; i++)
+                {
+                    // termination
+
+                    var cur = q.Dequeue();
+
+                    if (cur == target) { reached = true; break; }
+
+                    var nexts = findNeighbors(cur);
+
+                    foreach (var next in nexts)
+                    {
+                        if (!uplayerset.Contains(next))
+                        {
+                            // add it into adj list
+
+                            if (!nextlayerset.Contains(next))
+                            {
+                                nextlayerset.Add(next);
+                                q.Enqueue(next);
+                            }
+                        }
+                    }
+                }
+
+                foreach (var nl in nextlayerset)
+                {
+                    uplayerset.Add(nl);
+                }
+            }
+
+            return reached ? depth : 0;
+        }
+
+        private List<int> findNeighbors(int iword)
+        {
+            var res = new List<int>();
+            var word = wordlist[iword].ToCharArray();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                var oldchar = word[i];
+
+                for (char j = 'a'; j <= 'z'; j++)
+                {
+                    word[i] = j;
+                    if (j != oldchar && map.TryGetValue(new string(word), out int index) && index != iword)
+                    {
+                        res.Add(index);
+                    }
+                }
+
+                word[i] = oldchar;
+            }
+
+            return res;
+        }
+    }
+}
