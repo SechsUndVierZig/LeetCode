da2ce5a89b5f2d75d79077500a8f963924a33806
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-02 14:52:23 +0300

    Assignment128
    Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
    
    You must write an algorithm that runs in O(n) time.
    
    Example 1:
    
    Input: nums = [100,4,200,1,3,2]
    Output: 4
    Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
    Example 2:
    
    Input: nums = [0,3,7,2,5,8,4,6,0,1]
    Output: 9

---
+++ LeetCode/Assignment128.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment128
+{
+    public class Solution
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>(nums);
+            int maxLength = 0;
+
+            foreach (int num in nums)
+            {
+                if (set.Contains(num - 1)) continue;
+
+                int length = 0;
+                while (set.Contains(num + length)) length++;
+
+                maxLength = Math.Max(maxLength, length);
+            }
+
+            return maxLength;
+        }
+    }
+}
