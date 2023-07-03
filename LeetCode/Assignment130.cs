2c81d583823e8ba403657db71f698bcf2a1147c3
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-03 14:59:29 +0300

    Assignment130
    Given an m x n matrix board containing 'X' and 'O', capture all regions that are 4-directionally surrounded by 'X'.
    
    A region is captured by flipping all 'O's into 'X's in that surrounded region.
    
    Example 1:
    
    Input: board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
    Output: [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
    Explanation: Notice that an 'O' should not be flipped if:
    - It is on the border, or
    - It is adjacent to an 'O' that should not be flipped.
    The bottom 'O' is on the border, so it is not flipped.
    The other three 'O' form a surrounded region, so they are flipped.
    Example 2:
    
    Input: board = [["X"]]
    Output: [["X"]]

---
+++ LeetCode/Assignment130.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment130
+{
+    public class Solution
+    {
+        private const char X = 'X';
+        private const char O = 'O';
+        private const char Temp = 'T';
+
+        public void Solve(char[][] grid)
+        {
+            int m = grid.Length;
+            int n = grid[0].Length;
+
+            if (m == 1 || n == 1) return;
+
+            for (int cidx = 0; cidx < n; cidx++)
+            {
+                Visit(0, cidx, grid);
+                Visit(m - 1, cidx, grid);
+            }
+
+            for (int ridx = 1; ridx < m - 1; ridx++)
+            {
+                Visit(ridx, 0, grid);
+                Visit(ridx, n - 1, grid);
+            }
+
+            for (int ridx = 0; ridx < m; ridx++)
+            {
+                for (int cidx = 0; cidx < n; cidx++)
+                {
+                    grid[ridx][cidx] = grid[ridx][cidx] switch
+                    {
+                        O => X,
+                        Temp => O,
+                        _ => grid[ridx][cidx]
+                    };
+                }
+            }
+        }
+
+        private void Visit(int r, int c, char[][] grid)
+        {
+            if (r < 0 || r >= grid.Length || c < 0 || c >= grid[0].Length)
+            {
+                return;
+            }
+            else if (grid[r][c] == X || grid[r][c] == Temp)
+            {
+                return;
+            }
+
+            grid[r][c] = Temp;
+
+            foreach (var (x, y) in GetNeighbours(r, c))
+            {
+                Visit(x, y, grid);
+            }
+        }
+
+        private IEnumerable<(int, int c)> GetNeighbours(int r, int c)
+        {
+            yield return (r + 1, c);
+            yield return (r - 1, c);
+            yield return (r, c + 1);
+            yield return (r, c - 1);
+        }
+    }
+}
