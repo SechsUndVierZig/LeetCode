b04a0dd8ca70bcb3b83b31a6130ad04260d0a1d9
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-06 13:47:24 +0300

    Assignment135
    There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
    
    You are giving candies to these children subjected to the following requirements:
    
    Each child must have at least one candy.
    Children with a higher rating get more candies than their neighbors.
    Return the minimum number of candies you need to have to distribute the candies to the children.
    
    Example 1:
    
    Input: ratings = [1,0,2]
    Output: 5
    Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
    Example 2:
    
    Input: ratings = [1,2,2]
    Output: 4
    Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
    The third child gets 1 candy because it satisfies the above two conditions.

---
+++ LeetCode/Assignment135.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Assignment135
+{
+    public class Solution
+    {
+        public int Candy(int[] ratings)
+        {
+            int maxVal = 1;
+            int val = 1;
+            int ans = 1;
+            int maxIdx = 0;
+            for (int i = 1; i < ratings.Length; i++)
+            {
+                if (ratings[i] > ratings[i - 1])
+                {
+                    ++val;
+                    ans += val;
+                    maxIdx = i;
+                    maxVal = val;
+                }
+                else if (ratings[i] < ratings[i - 1])
+                {
+                    val = 1;
+                    ans += i - maxIdx;
+                    if (i - maxIdx == maxVal)
+                    {
+                        maxVal += 1;
+                        ans += 1;
+                    }
+                }
+                else
+                {
+                    maxVal = 1;
+                    val = 1;
+                    maxIdx = i;
+                    ans += 1;
+                }
+            }
+            return ans;
+
+
+
+        }
+    }
+}
