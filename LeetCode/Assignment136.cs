4545d920a000a8d5342aa324fb46fa0511aaffe4
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-07 12:54:40 +0300

    Assignment136
    Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
    
    You must implement a solution with a linear runtime complexity and use only constant extra space.
    
    Example 1:
    
    Input: nums = [2,2,1]
    Output: 1
    Example 2:
    
    Input: nums = [4,1,2,1,2]
    Output: 4
    Example 3:
    
    Input: nums = [1]
    Output: 1

---
+++ LeetCode/Assignment136.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Assignment136
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            List<int> lst = new List<int>();
+            for (int i = 0; i < nums.Count(); i++)
+            {
+                if (lst.Contains(nums[i]))
+                    lst.Remove(nums[i]);
+                else
+                    lst.Add(nums[i]);
+            }
+            return lst[0];
+        }
+    }
+}
