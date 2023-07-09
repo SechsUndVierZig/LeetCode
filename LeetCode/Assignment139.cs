ba4d686e8cc74b2140f5452d7a0ab7d669912730
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-09 16:07:50 +0300

    Assignment139
    Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
    
    Note that the same word in the dictionary may be reused multiple times in the segmentation.
    
    Example 1:
    
    Input: s = "leetcode", wordDict = ["leet","code"]
    Output: true
    Explanation: Return true because "leetcode" can be segmented as "leet code".
    Example 2:
    
    Input: s = "applepenapple", wordDict = ["apple","pen"]
    Output: true
    Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
    Note that you are allowed to reuse a dictionary word.
    Example 3:
    
    Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
    Output: false

---
+++ LeetCode/Assignment139.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Assignment139
+{
+    public class Solution
+    {
+        List<string> _wordDict;
+        HashSet<string> _failed = new();
+
+        public bool WordBreak(string s, IList<string> wordDict)
+        {
+            _wordDict = new List<string>(wordDict);
+            return WordBreakInternal(s);
+        }
+
+        private bool WordBreakInternal(string s)
+        {
+            if (s.Length == 0)
+                return true;
+            if (_failed.Contains(s))
+                return false;
+
+            foreach (var word in _wordDict)
+            {
+                if (s.StartsWith(word) && WordBreakInternal(s.Substring(word.Length)))
+                    return true;
+            }
+            _failed.Add(s);
+            return false;
+        }
+    }
+}
