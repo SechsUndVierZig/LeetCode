27c587c47f8bc24f06a14d00157be7a73ad9f0ae
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-10 13:07:23 +0300

    Assignment140
    Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
    
    Note that the same word in the dictionary may be reused multiple times in the segmentation.
    
    Example 1:
    
    Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
    Output: ["cats and dog","cat sand dog"]
    Example 2:
    
    Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
    Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
    Explanation: Note that you are allowed to reuse a dictionary word.
    Example 3:
    
    Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
    Output: []

---
+++ LeetCode/Assignment140.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment140
+{
+    public class Solution
+    {
+        public IList<string> WordBreak(string s, IList<string> wordDict)
+        {
+            IList<string> result = new List<string>();
+
+            DFSWordBreak(0, s, "", wordDict, "", result);
+
+            return result;
+        }
+
+        public void DFSWordBreak(int index, string s, string word, IList<string> wordDict, string current, IList<string> result)
+        {
+            if (index == s.Length)
+            {
+                result.Add(current.Substring(1));
+                return;
+            }
+
+            for (int i = index; i < s.Length; i++)
+            {
+                var currentW = s.Substring(index, i - index + 1);
+                if (wordDict.Contains(currentW))
+                {
+                    current += " " + currentW;
+                    DFSWordBreak(i + 1, s, "", wordDict, current, result);
+                    current = current.Substring(0, current.Length - (currentW.Length + 1));
+                }
+            }
+        }
+    }
+}
