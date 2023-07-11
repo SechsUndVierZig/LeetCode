2d30bc9149276f1cfe4a18c4d438b612e87476ec
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-11 13:06:46 +0300

    Assignment142
    Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
    
    There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
    
    Do not modify the linked list.
    
    Example 1:
    
    Input: head = [3,2,0,-4], pos = 1
    Output: tail connects to node index 1
    Explanation: There is a cycle in the linked list, where tail connects to the second node.
    Example 2:
    
    Input: head = [1,2], pos = 0
    Output: tail connects to node index 0
    Explanation: There is a cycle in the linked list, where tail connects to the first node.
    Example 3:
    
    Input: head = [1], pos = -1
    Output: no cycle
    Explanation: There is no cycle in the linked list.

---
+++ LeetCode/Assignment142.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment142
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            var set = new HashSet<ListNode>();
+
+            while (head != null)
+            {
+                if (set.Contains(head))
+                    break;
+
+                set.Add(head);
+                head = head.next;
+            }
+
+            return head;
+        }
+    }
+}
