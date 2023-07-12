c8171a746dd00e76f60555a80824213beb17bb1e
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-12 12:20:46 +0300

    Assignment144
    Given the root of a binary tree, return the preorder traversal of its nodes' values.
    
    Example 1:
    
    Input: root = [1,null,2,3]
    Output: [1,2,3]
    Example 2:
    
    Input: root = []
    Output: []
    Example 3:
    
    Input: root = [1]
    Output: [1]

---
+++ LeetCode/Assignment144.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment144
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            var list = new List<int>();
+            PreorderTraversalRecursive(root, list);
+            return list;
+        }
+
+        private void PreorderTraversalRecursive(TreeNode node, List<int> list)
+        {
+            if (node == null) return;
+            list.Add(node.val);
+            PreorderTraversalRecursive(node.left, list);
+            PreorderTraversalRecursive(node.right, list);
+        }
+    }
+}
