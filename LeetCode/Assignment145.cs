187fd383199d2720e5d44d014d24aec4e4f9913c
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-13 13:18:05 +0300

    Assignment145
    Given the root of a binary tree, return the postorder traversal of its nodes' values.
    
    Example 1:
    
    Input: root = [1,null,2,3]
    Output: [3,2,1]
    Example 2:
    
    Input: root = []
    Output: []
    Example 3:
    
    Input: root = [1]
    Output: [1]

---
+++ LeetCode/Assignment145.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment145
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            if (root == null)
+                return new List<int>();
+            List<int> result = new List<int>();
+            if (root.left != null)
+            {
+                result.AddRange(PostorderTraversal(root.left));
+            }
+            if (root.right != null)
+            {
+                result.AddRange(PostorderTraversal(root.right));
+            }
+            result.Add(root.val);
+
+            return result;
+        }
+    }
+}
