093a3e76de646dc0dd360f8ed43a778c4446bc8f
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-15 14:06:40 +0300

    Assignment149
    Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight line.
    
    Example 1:
    
    Input: points = [[1,1],[2,2],[3,3]]
    Output: 3
    Example 2:
    
    Input: points = [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
    Output: 4

---
+++ LeetCode/Assignment149.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment149
+{
+    public class Solution
+    {
+        public int MaxPoints(int[][] points)
+        {
+            int n = points.Length;
+            if (n == 1) return 1;
+            int result = 2;
+            for (int i = 0; i < n; ++i)
+            {
+                var cnt = new Dictionary<double, int>();
+                for (int j = 0; j < n; ++j)
+                {
+                    if (j != i)
+                    {
+                        var angle = Math.Atan2(points[j][1] - points[i][1], points[j][0] - points[i][0]);
+                        cnt[angle] = cnt.GetValueOrDefault(angle, 0) + 1;
+                    }
+                }
+                result = Math.Max(result, cnt.Values.Max() + 1);
+            }
+            return result;
+        }
+    }
+}
+}
