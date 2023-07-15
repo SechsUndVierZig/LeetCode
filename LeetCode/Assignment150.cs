01f77d6ae997fc61173ea875b2a2db82fac31434
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-15 14:08:51 +0300

    Assignment150
    You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
    
    Evaluate the expression. Return an integer that represents the value of the expression.
    
    Note that:
    
    The valid operators are '+', '-', '*', and '/'.
    Each operand may be an integer or another expression.
    The division between two integers always truncates toward zero.
    There will not be any division by zero.
    The input represents a valid arithmetic expression in a reverse polish notation.
    The answer and all the intermediate calculations can be represented in a 32-bit integer.
    
    Example 1:
    
    Input: tokens = ["2","1","+","3","*"]
    Output: 9
    Explanation: ((2 + 1) * 3) = 9
    Example 2:
    
    Input: tokens = ["4","13","5","/","+"]
    Output: 6
    Explanation: (4 + (13 / 5)) = 6
    Example 3:
    
    Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
    Output: 22
    Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
    = ((10 * (6 / (12 * -11))) + 17) + 5
    = ((10 * (6 / -132)) + 17) + 5
    = ((10 * 0) + 17) + 5
    = (0 + 17) + 5
    = 17 + 5
    = 22

---
+++ LeetCode/Assignment150.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment150
+{
+    public class Solution
+    {
+        private static Dictionary<string, Func<int, int, int>> s_Funcs = new() {
+      { "+", (a, b) => a + b },
+      { "-", (a, b) => b - a },
+      { "*", (a, b) => a * b },
+      { "/", (a, b) => b / a },
+    };
+        public int EvalRPN(string[] tokens)
+        {
+            Stack<int> data = new();
+
+            foreach (string token in tokens)
+                if (int.TryParse(token, out int value))
+                    data.Push(value);
+                else
+                    data.Push(s_Funcs[token](data.Pop(), data.Pop()));
+
+            return data.Pop();
+        }
+    }
+}
