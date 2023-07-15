faf48c7f21d2e31e5025ccb8a8e1b2ced2c70922
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-15 13:43:35 +0300

    Assignment147
    Given the head of a singly linked list, sort the list using insertion sort, and return the sorted list's head.
    
    The steps of the insertion sort algorithm:
    
    Insertion sort iterates, consuming one input element each repetition and growing a sorted output list.
    At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list and inserts it there.
    It repeats until no input elements remain.
    The following is a graphical example of the insertion sort algorithm. The partially sorted list (black) initially contains only the first element in the list. One element (red) is removed from the input data and inserted in-place into the sorted list with each iteration.
    
    Example 1:
    
    Input: head = [4,2,1,3]
    Output: [1,2,3,4]
    Example 2:
    
    Input: head = [-1,5,3,4,0]
    Output: [-1,0,3,4,5]

---
+++ LeetCode/Assignment147.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Assignment147
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public ListNode InsertionSortList(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+            ListNode sortedlist = null;
+            ListNode clone = head;
+            while (clone != null)
+            {
+                if (sortedlist == null || clone.val < sortedlist.val)
+                {
+                    var temp = new ListNode(clone.val, sortedlist);
+                    sortedlist = temp;
+                }
+                else
+                {
+                    ListNode current = sortedlist;
+                    while (current.next != null && current.next.val < clone.val)
+                        current = current.next;
+                    var temp = new ListNode(clone.val, current.next);
+                    current.next = temp;
+                }
+                clone = clone.next;
+            }
+            return sortedlist;
+        }
+    }
+}
