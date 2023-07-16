0306036972b187a4954a05bf5d1e371287c6b42d
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-16 15:02:10 +0300

    Assignment151
    Given an input string s, reverse the order of the words.
    
    A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
    
    Return a string of the words in reverse order concatenated by a single space.
    
    Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
    
    Example 1:
    
    Input: s = "the sky is blue"
    Output: "blue is sky the"
    Example 2:
    
    Input: s = "  hello world  "
    Output: "world hello"
    Explanation: Your reversed string should not contain leading or trailing spaces.
    Example 3:
    
    Input: s = "a good   example"
    Output: "example good a"
    Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.

---
+++ LeetCode/Assignment151.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public class Assignment151
+{
+    public class Solution
+    {
+        public string ReverseWords(string s)
+        {
+            return String.Join(" ", s.Split(' ', StringSplitOptions.RemoveEmptyEntries).Reverse());
+        }
+    }
+}
