fc009dfa9fc32c465a0896f0aed64fdd2c282ad9
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-18 14:14:43 +0300

    Assignment154
    Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,4,4,5,6,7] might become:
    
    [4,5,6,7,0,1,4] if it was rotated 4 times.
    [0,1,4,4,5,6,7] if it was rotated 7 times.
    Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
    
    Given the sorted rotated array nums that may contain duplicates, return the minimum element of this array.
    
    You must decrease the overall operation steps as much as possible.
    
    Example 1:
    
    Input: nums = [1,3,5]
    Output: 1
    Example 2:
    
    Input: nums = [2,2,2,0,1]
    Output: 0

---
+++ LeetCode/Assignment154.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public class Assignment154
+{
+    public class Solution
+    {
+        public int FindMin(int[] nums)
+        {
+            return (nums.Min());
+        }
+    }
+}
