e8e56781d12ba8b5403c381e7698b27277001ab7
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-21 14:04:32 +0300

    Assignment164
    Given an integer array nums, return the maximum difference between two successive elements in its sorted form. If the array contains less than two elements, return 0.
    
    You must write an algorithm that runs in linear time and uses linear extra space.
    
    Example 1:
    
    Input: nums = [3,6,9,1]
    Output: 3
    Explanation: The sorted form of the array is [1,3,6,9], either (3,6) or (6,9) has the maximum difference 3.
    Example 2:
    
    Input: nums = [10]
    Output: 0
    Explanation: The array contains less than 2 elements, therefore return 0.

---
+++ LeetCode/Assignment164.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Assignment164
+{
+    public class Solution
+    {
+        public int MaximumGap(int[] nums)
+        {
+            if (nums.Length < 2)
+                return 0;
+            var queue = new PriorityQueue<int, int>(nums.Select(z => (z, z)));
+
+            var gap = 0;
+
+            while (queue.Count != 1)
+            {
+                var cgap = -queue.Dequeue() + queue.Peek();
+                if (gap < cgap)
+                    gap = cgap;
+            }
+
+            return gap;
+        }
+    }
+}
