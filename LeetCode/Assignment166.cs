a73d669f9c357e2182ee0372688d080a67ffedb5
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-22 13:12:35 +0300

    Assignment166
    Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
    
    If the fractional part is repeating, enclose the repeating part in parentheses.
    
    If multiple answers are possible, return any of them.
    
    It is guaranteed that the length of the answer string is less than 104 for all the given inputs.
    
    Example 1:
    
    Input: numerator = 1, denominator = 2
    Output: "0.5"
    Example 2:
    
    Input: numerator = 2, denominator = 1
    Output: "2"
    Example 3:
    
    Input: numerator = 4, denominator = 333
    Output: "0.(012)"

---
+++ LeetCode/Assignment166.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Assignment166
+{
+    public class Solution
+    {
+        public string FractionToDecimal(int numerator, int denominator)
+        {
+            bool isNeg = true;
+            if ((numerator < 0 && denominator < 0) || 
+                (numerator > 0 && denominator > 0) || 
+                numerator == 0)
+                isNeg = false;
+
+            StringBuilder sb = new StringBuilder();
+            if (isNeg)
+                sb.Append("-");
+
+            long num = Math.Abs((long)numerator);
+            long den = Math.Abs((long)denominator);
+
+            sb.Append(num / den);
+            num %= den;
+
+            if (num > 0)
+            {
+                Dictionary<double, int> d = new Dictionary<double, int>();
+                sb.Append(".");
+
+                while (num > 0 && (!d.ContainsKey(num)))
+                {
+                    d.Add(num, sb.Length);
+                    num *= 10;
+                    sb.Append(num / den);
+                    num %= den;
+                }
+
+                if (d.ContainsKey(num))
+                {
+                    sb.Insert(d[num], '(');
+                    sb.Append(")");
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
