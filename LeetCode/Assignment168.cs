1cb08290f5eb342812133d642b0f356c6c8c8b99
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-23 13:39:17 +0300

    Assignment168
    Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
    
    For example:
    
    A -> 1
    B -> 2
    C -> 3
    ...
    Z -> 26
    AA -> 27
    AB -> 28
    ...
    
    Example 1:
    
    Input: columnNumber = 1
    Output: "A"
    Example 2:
    
    Input: columnNumber = 28
    Output: "AB"
    Example 3:
    
    Input: columnNumber = 701
    Output: "ZY"

---
+++ LeetCode/Assignment168.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+public class Assignment168
+{
+    public class Solution
+    {
+        public string ConvertToTitle(int columnNumber)
+        {
+            const int BASE_26 = 26;
+            var sb = new StringBuilder();
+            var residuo = 0;
+
+            while (columnNumber > 0)
+            {
+                columnNumber--;
+                residuo = columnNumber % BASE_26;
+                sb.Insert(0, GetCharacter(residuo));
+                columnNumber /= BASE_26;
+            }
+
+            return sb.ToString();
+        }
+
+        private static char GetCharacter(int columnNumber) => (char)('A' + columnNumber);
+    }
+}
