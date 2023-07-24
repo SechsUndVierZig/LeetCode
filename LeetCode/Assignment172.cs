4451dc7048278cbffa92186a46fbd7f2cd1d7c9d
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-24 13:44:36 +0300

    Assignment172
    Given an integer n, return the number of trailing zeroes in n!.
    
    Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.
    
    Example 1:
    
    Input: n = 3
    Output: 0
    Explanation: 3! = 6, no trailing zero.
    Example 2:
    
    Input: n = 5
    Output: 1
    Explanation: 5! = 120, one trailing zero.
    Example 3:
    
    Input: n = 0
    Output: 0

---
+++ LeetCode/Assignment172.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Assignment172
+{
+    public class Solution
+    {
+        public int TrailingZeroes(int n)
+        {
+            var count = 0;
+            for (var i = 5; i <= n; i *= 5) count += n / i;
+
+            return count;
+        }
+    }
+}
