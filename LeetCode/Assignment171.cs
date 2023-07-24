c044ec97e4850725a3c9bbd271f4dc742dde1610
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-24 12:59:47 +0300

    Assignment171
    Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
    
    For example:
    
    A -> 1
    B -> 2
    C -> 3
    ...
    Z -> 26
    AA -> 27
    AB -> 28
    ...
    
    Example 1:
    
    Input: columnTitle = "A"
    Output: 1
    Example 2:
    
    Input: columnTitle = "AB"
    Output: 28
    Example 3:
    
    Input: columnTitle = "ZY"
    Output: 701

---
+++ LeetCode/Assignment171.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Assignment171
+{
+    public class Solution
+    {
+        public int TitleToNumber(string columnTitle)
+        {
+            int output = 0;
+            for (int i = 0; i < columnTitle.Length; i++)
+            {
+                int curr = columnTitle[i] - 'A' + 1;
+                output = output * 26 + curr;
+            }
+            return output;
+        }
+    }
+}
