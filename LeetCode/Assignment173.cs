c16a48e6e6a3e6a33f72e70be9a0365caf0834f5
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-24 13:50:56 +0300

    Assignment173
    Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
    
    BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
    boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
    int next() Moves the pointer to the right, then returns the number at the pointer.
    Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
    
    You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
    
    Example 1:
    
    Input
    ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
    [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
    Output
    [null, 3, 7, true, 9, true, 15, true, 20, false]
    
    Explanation
    BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
    bSTIterator.next();    // return 3
    bSTIterator.next();    // return 7
    bSTIterator.hasNext(); // return True
    bSTIterator.next();    // return 9
    bSTIterator.hasNext(); // return True
    bSTIterator.next();    // return 15
    bSTIterator.hasNext(); // return True
    bSTIterator.next();    // return 20
    bSTIterator.hasNext(); // return False

---
+++ LeetCode/Assignment173.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment173
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    public class BSTIterator
+    {
+
+        List<int> values = new List<int>();
+
+        public BSTIterator(TreeNode root)
+        {
+            PreOrder(root, values);
+        }
+
+        public int Next()
+        {
+            if (values.Any())
+            {
+                var temp = values[0];
+                values.RemoveAt(0);
+                return temp;
+            }
+            return 0;
+        }
+
+        public bool HasNext()
+        {
+
+            return values.Any();
+        }
+
+        private void PreOrder(TreeNode root, List<int> values)
+        {
+            if (root != null)
+            {
+                PreOrder(root.left, values);
+
+                values.Add(root.val);
+                PreOrder(root.right, values);
+
+            }
+            return;
+        }
+    }
+
+    /**
+     * Your BSTIterator object will be instantiated and called as such:
+     * BSTIterator obj = new BSTIterator(root);
+     * int param_1 = obj.Next();
+     * bool param_2 = obj.HasNext();
+     */
+}
