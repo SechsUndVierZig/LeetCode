d7337325c5a99e9f4ffb28368a9bd7385382d4f5
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-25 16:23:35 +0300

    Assignment179
    Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
    
    Since the result may be very large, so you need to return a string instead of an integer.
    
    Example 1:
    
    Input: nums = [10,2]
    Output: "210"
    Example 2:
    
    Input: nums = [3,30,34,5,9]
    Output: "9534330"

---
+++ LeetCode/Assignment179.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public class Assignment179
+{
+    public class Solution
+    {
+        public string LargestNumber(int[] nums)
+        {
+            if (nums.All(_ => _ == 0)) return "0";
+
+            var s = nums.Select(_ => _.ToString()).ToList();
+
+            s.Sort((a, b) => (b + a).CompareTo(a + b));
+
+            return string.Concat(s);
+        }
+    }
+}
