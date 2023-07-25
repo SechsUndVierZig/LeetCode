52d3bc1a0c6911946a1659a1f041dc0b4e676ef1
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-25 16:18:33 +0300

    Assignment174
    The demons had captured the princess and imprisoned her in the bottom-right corner of a dungeon. The dungeon consists of m x n rooms laid out in a 2D grid. Our valiant knight was initially positioned in the top-left room and must fight his way through dungeon to rescue the princess.
    
    The knight has an initial health point represented by a positive integer. If at any point his health point drops to 0 or below, he dies immediately.
    
    Some of the rooms are guarded by demons (represented by negative integers), so the knight loses health upon entering these rooms; other rooms are either empty (represented as 0) or contain magic orbs that increase the knight's health (represented by positive integers).
    
    To reach the princess as quickly as possible, the knight decides to move only rightward or downward in each step.
    
    Return the knight's minimum initial health so that he can rescue the princess.
    
    Note that any room can contain threats or power-ups, even the first room the knight enters and the bottom-right room where the princess is imprisoned.
    
    Example 1:
    
    Input: dungeon = [[-2,-3,3],[-5,-10,1],[10,30,-5]]
    Output: 7
    Explanation: The initial health of the knight must be at least 7 if he follows the optimal path: RIGHT-> RIGHT -> DOWN -> DOWN.
    Example 2:
    
    Input: dungeon = [[0]]
    Output: 1

---
+++ LeetCode/Assignment174.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Assignment174
+{
+    public class Solution
+    {
+        public int CalculateMinimumHP(int[][] dungeon)
+        {
+            int r = dungeon.Length;
+            int c = dungeon[0].Length;
+            int[,] dp = InitializeDPArray(r, c);
+
+            dp[r - 1, c - 1] = Math.Max(1, 1 - dungeon[r - 1][c - 1]);
+
+            for (int i = c - 2; i >= 0; i--)
+                dp[r - 1, i] = Math.Max(1, dp[r - 1, i + 1] - dungeon[r - 1][i]);
+            for (int i = r - 2; i >= 0; i--)
+                dp[i, c - 1] = Math.Max(1, dp[i + 1, c - 1] - dungeon[i][c - 1]);
+            for (int i = r - 2; i >= 0; i--)
+            {
+                for (int j = c - 2; j >= 0; j--)
+                {
+                    int min = Math.Min(dp[i + 1, j], dp[i, j + 1]);
+                    dp[i, j] = Math.Max(1, min - dungeon[i][j]);
+                }
+            }
+
+            return dp[0, 0];
+        }
+
+        private int[,] InitializeDPArray(int r, int c)
+        {
+            int[,] dp = new int[r, c];
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                    dp[i, j] = int.MaxValue;
+            }
+
+            return dp;
+        }
+    }
+}
