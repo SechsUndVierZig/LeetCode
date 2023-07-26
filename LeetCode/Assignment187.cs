f4a6e6d02e2cd3a409bcc06e0d092aa83471bee5
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-26 16:08:08 +0300

    Assignment187
    The DNA sequence is composed of a series of nucleotides abbreviated as 'A', 'C', 'G', and 'T'.
    
    For example, "ACGAATTCCG" is a DNA sequence.
    When studying DNA, it is useful to identify repeated sequences within the DNA.
    
    Given a string s that represents a DNA sequence, return all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule. You may return the answer in any order.
    
    Example 1:
    
    Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
    Output: ["AAAAACCCCC","CCCCCAAAAA"]
    Example 2:
    
    Input: s = "AAAAAAAAAAAAA"
    Output: ["AAAAAAAAAA"]

---
+++ LeetCode/Assignment187.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Assignment187
+{
+    public class Solution
+    {
+        public IList<string> FindRepeatedDnaSequences(string s)
+        {
+            var dic = new Dictionary<string, int>();
+            for (int i = 0; i < s.Length - 9; i++)
+            {
+                var key = s.Substring(i, 10);
+                if (!dic.ContainsKey(key))
+                    dic.Add(key, 1);
+                else
+                    dic[key]++;
+            }
+            var rs = new List<string>();
+            foreach (var item in dic)
+                if (item.Value > 1) rs.Add(item.Key);
+            return rs;
+        }
+    }
+}
