1115ec2a6ebb6bb5aea2dd4869950e717ad304e7
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-27 12:59:24 +0300

    Assignment188
    You are given an integer array prices where prices[i] is the price of a given stock on the ith day, and an integer k.
    
    Find the maximum profit you can achieve. You may complete at most k transactions: i.e. you may buy at most k times and sell at most k times.
    
    Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
    
    Example 1:
    
    Input: k = 2, prices = [2,4,1]
    Output: 2
    Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
    Example 2:
    
    Input: k = 2, prices = [3,2,6,5,0,3]
    Output: 7
    Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4. Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.

---
+++ LeetCode/Assignment188.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Assignment188
+{
+    public class Solution
+    {
+        private int[,] dp;
+        public int MaxProfit(int k, int[] prices)
+        {
+
+            dp = new int[prices.Length + 1, 2 * k + 1];
+
+            System.Threading.Tasks.Parallel.For(0, prices.Length + 1, x => {
+                for (int y = 0; y < 2 * k + 1; ++y)
+                    dp[x, y] = -1;
+            });
+
+            return solve(0, 2 * k, prices);
+        }
+
+        private int solve(int i, int tno, int[] p)
+        {
+            if (i == p.Length || tno == 0) return 0;
+            if (dp[i, tno] != -1) return dp[i, tno];
+
+            if (tno % 2 == 0)
+                return dp[i, tno] = Math.Max(-p[i] + solve(i + 1, tno - 1, p), solve(i + 1, tno, p));
+
+
+            return dp[i, tno] = Math.Max(p[i] + solve(i + 1, tno - 1, p), solve(i + 1, tno, p));
+        }
+    }
+}
