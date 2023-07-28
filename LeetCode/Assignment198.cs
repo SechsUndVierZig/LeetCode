3a02e429fe1ed2b20fed297c155fd49005e3e2da
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-28 14:40:11 +0300

    Assignment198
    You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
    
    Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
    
    Example 1:
    
    Input: nums = [1,2,3,1]
    Output: 4
    Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
    Total amount you can rob = 1 + 3 = 4.
    Example 2:
    
    Input: nums = [2,7,9,3,1]
    Output: 12
    Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
    Total amount you can rob = 2 + 9 + 1 = 12.

---
+++ LeetCode/Assignment198.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Assignment198
+{
+    public class Solution
+    {
+        public int Rob(int[] nums)
+        {
+            int max = nums[0];
+            int s = 0;
+
+            foreach (int n in nums[1..])
+                (max, s) = (Math.Max(n + s, max), max);
+
+            return max;
+        }
+    }
+}
