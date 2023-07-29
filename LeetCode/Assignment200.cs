ccb8e8380cb54c847344a1d6835e14070324d526
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-29 15:00:57 +0300

    Assignment200
    Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
    
    An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
    
    Example 1:
    
    Input: grid = [
      ["1","1","1","1","0"],
      ["1","1","0","1","0"],
      ["1","1","0","0","0"],
      ["0","0","0","0","0"]
    ]
    Output: 1
    Example 2:
    
    Input: grid = [
      ["1","1","0","0","0"],
      ["1","1","0","0","0"],
      ["0","0","1","0","0"],
      ["0","0","0","1","1"]
    ]
    Output: 3

---
+++ LeetCode/Assignment200.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment200
+{
+    public class Solution
+    {
+        public int NumIslands(char[][] grid)
+        {
+            Queue<(int, int)> queue = new();
+            (int, int)[] directions = new[] { (-1, 0), (1, 0), (0, -1), (0, 1) };
+
+            int result = 0;
+
+            for (int i = 0; i < grid.Length; i++)
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == '1')
+                    {
+                        result++;
+                        queue.Enqueue((i, j));
+                        while (queue.Count > 0)
+                        {
+                            var p = queue.Dequeue();
+                            foreach (var dir in directions)
+                            {
+                                int r = p.Item1 + dir.Item1;
+                                int c = p.Item2 + dir.Item2;
+
+                                if (r >= 0 && r < grid.Length &&
+                                   c >= 0 && c < grid[r].Length &&
+                                   grid[r][c] == '1')
+                                {
+                                    grid[r][c] = '2';
+                                    queue.Enqueue((r, c));
+                                }
+                            }
+                        }
+                    }
+                }
+
+            return result;
+        }
+    }
+}
