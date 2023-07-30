e76c1a73ffb265c66652a1d1c08450fb45a6d02c
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-30 16:05:18 +0300

    Assignment203
    Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
    
    Example 1:
    
    Input: head = [1,2,6,3,4,5,6], val = 6
    Output: [1,2,3,4,5]
    Example 2:
    
    Input: head = [], val = 1
    Output: []
    Example 3:
    
    Input: head = [7,7,7,7], val = 7
    Output: []

---
+++ LeetCode/Assignment203.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Assignment203
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            if (head == null)
+                return null;
+            ListNode root = head;
+            ListNode prev = null;
+            while (head != null)
+            {
+                if (head.val == val)
+                {
+                    if (prev == null)
+                        root = head.next;
+                    else
+                        prev.next = head.next;
+                    head = head.next;
+                    continue;
+                }
+                prev = head;
+                head = head.next;
+            }
+            return root;
+        }
+    }
+}
