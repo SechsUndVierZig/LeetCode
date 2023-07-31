a4c8eda5a72dd9eb5c295448cb1781228704f814
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-07-31 10:58:29 +0300

    Assignment204
    Given an integer n, return the number of prime numbers that are strictly less than n.
    
    Example 1:
    
    Input: n = 10
    Output: 4
    Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
    Example 2:
    
    Input: n = 0
    Output: 0
    Example 3:
    
    Input: n = 1
    Output: 0

---
+++ LeetCode/Assignment204.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Assignment204
+{
+    public class Solution
+    {
+        public int CountPrimes(int n)
+        {
+            if (n <= 2) return 0;
+            int result = 0;
+            bool[] sieve = new bool[n];
+            for (int i = 2; i < n; i++)
+            {
+                if (sieve[i]) continue;
+                result++;
+
+                int count = 2;
+                while (i * count < n)
+                {
+                    sieve[i * count] = true;
+                    count++;
+                }
+            }
+            return result;
+        }
+    }
+}
