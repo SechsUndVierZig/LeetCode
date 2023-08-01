e959718260c97cdeda1277a91a4a1abbb3847463
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-08-01 13:15:24 +0300

    Assignment205
    Given two strings s and t, determine if they are isomorphic.
    
    Two strings s and t are isomorphic if the characters in s can be replaced to get t.
    
    All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
    
    Example 1:
    
    Input: s = "egg", t = "add"
    Output: true
    Example 2:
    
    Input: s = "foo", t = "bar"
    Output: false
    Example 3:
    
    Input: s = "paper", t = "title"
    Output: true

---
+++ LeetCode/Assignment205.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment205
+{
+    public class Solution
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            Dictionary<char, char> sToT = new Dictionary<char, char>();
+            Dictionary<char, char> tToS = new Dictionary<char, char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char sChar = s[i];
+                char tChar = t[i];
+
+                if (sToT.ContainsKey(sChar))
+                {
+                    if (sToT[sChar] != tChar)
+                        return false;
+                }
+                else if (tToS.ContainsKey(tChar))
+                {
+                    if (tToS[tChar] != sChar)
+                        return false;
+                }
+                else
+                {
+                    sToT[sChar] = tChar;
+                    tToS[tChar] = sChar;
+                }
+            }
+
+            return true;
+        }
+    }
+}
