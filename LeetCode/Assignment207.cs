42212b23d507dc080e27ca2bdd5008cb32ee1f8c
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-08-03 12:38:53 +0300

    Assignment207
    There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
    
    For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
    Return true if you can finish all courses. Otherwise, return false.
    
    Example 1:
    
    Input: numCourses = 2, prerequisites = [[1,0]]
    Output: true
    Explanation: There are a total of 2 courses to take.
    To take course 1 you should have finished course 0. So it is possible.
    Example 2:
    
    Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
    Output: false
    Explanation: There are a total of 2 courses to take.
    To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.

---
+++ LeetCode/Assignment207.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment207
+{
+    public class Solution
+    {
+        public bool CanFinish(int numCourses, int[][] prerequisites)
+        {
+            List<List<int>> adjList = new List<List<int>>();
+            for (int i = 0; i < numCourses; i++)
+                adjList.Add(new List<int>());
+
+            // Populate adjacency list with prerequisites
+            foreach (int[] prerequisite in prerequisites)
+            {
+                int course = prerequisite[0];
+                int prerequisiteCourse = prerequisite[1];
+                adjList[course].Add(prerequisiteCourse);
+            }
+
+
+            bool[] visited = new bool[numCourses];
+            bool[] recursionStack = new bool[numCourses];
+
+            for (int course = 0; course < numCourses; course++)
+            {
+                if (HasCycle(course, adjList, visited, recursionStack))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool HasCycle(int course, List<List<int>> adjList, bool[] visited, bool[] recursionStack)
+        {
+            visited[course] = true;
+            recursionStack[course] = true;
+
+            foreach (int prerequisiteCourse in adjList[course])
+            {
+                if (recursionStack[prerequisiteCourse])
+                    return true;
+                if (!visited[prerequisiteCourse] && HasCycle(prerequisiteCourse, adjList, visited, recursionStack))
+                    return true;
+            }
+            recursionStack[course] = false;
+
+            return false;
+        }
+    }
+}
