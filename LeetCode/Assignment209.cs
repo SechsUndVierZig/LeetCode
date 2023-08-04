e860cf033409a525faab1716566427ad08ce436e
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-08-04 13:22:26 +0300

    Assignment209
    Given an array of positive integers nums and a positive integer target, return the minimal length of a
    subarray
     whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
    
    Example 1:
    
    Input: target = 7, nums = [2,3,1,2,4,3]
    Output: 2
    Explanation: The subarray [4,3] has the minimal length under the problem constraint.
    Example 2:
    
    Input: target = 4, nums = [1,4,4]
    Output: 1
    Example 3:
    
    Input: target = 11, nums = [1,1,1,1,1,1,1,1]
    Output: 0

---
+++ LeetCode/Assignment209.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Assignment209
+{
+    public class Solution
+    {
+        public int MinSubArrayLen(int target, int[] nums)
+        {
+            int minLength = int.MaxValue;
+            int sum = 0;
+            int left = 0;
+
+            for (int right = 0; right < nums.Length; right++)
+            {
+                sum += nums[right];
+
+                while (sum >= target)
+                {
+                    minLength = Math.Min(minLength, right - left + 1);
+                    sum -= nums[left];
+                    left++;
+                }
+            }
+
+            return minLength != int.MaxValue ? minLength : 0;
+        }
+    }
+}
