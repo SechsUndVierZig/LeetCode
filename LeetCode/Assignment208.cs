925f609fe5ebe24758f896273a891cb69447c3cc
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-08-04 13:20:46 +0300

    Assignment208
    A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
    
    Implement the Trie class:
    
    Trie() Initializes the trie object.
    void insert(String word) Inserts the string word into the trie.
    boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
    boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
    
    Example 1:
    
    Input
    ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
    [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
    Output
    [null, null, true, false, true, null, true]
    
    Explanation
    Trie trie = new Trie();
    trie.insert("apple");
    trie.search("apple");   // return True
    trie.search("app");     // return False
    trie.startsWith("app"); // return True
    trie.insert("app");
    trie.search("app");     // return True

---
+++ LeetCode/Assignment208.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment208
+{
+    public class Trie
+    {
+        HashSet<string> values;
+        HashSet<string> keys;
+        public Trie()
+        {
+            values = new HashSet<string>();
+            keys = new HashSet<string>();
+        }
+
+        public void Insert(string word)
+        {
+            if (!values.Contains(word))
+                values.Add(word);
+            for (var i = word.Length; i > 0; i--)
+            {
+                if (!keys.Contains(word.Substring(0, i)))
+                    keys.Add(word.Substring(0, i));
+                else
+                    break;// all prev string are in KEYS
+            }
+        }
+
+        public bool Search(string word)
+        {
+            return values.Contains(word);
+        }
+
+        public bool StartsWith(string prefix)
+        {
+            return keys.Contains(prefix);
+        }
+    }
+    /**
+     * Your Trie object will be instantiated and called as such:
+     * Trie obj = new Trie();
+     * obj.Insert(word);
+     * bool param_2 = obj.Search(word);
+     * bool param_3 = obj.StartsWith(prefix);
+     */
+}
