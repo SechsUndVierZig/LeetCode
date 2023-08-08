663b440602962101271140efd509380f83986112
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-08-08 16:33:44 +0300

    Assignment213
    You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
    
    Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
    
    Example 1:
    
    Input: nums = [2,3,2]
    Output: 3
    Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
    Example 2:
    
    Input: nums = [1,2,3,1]
    Output: 4
    Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
    Total amount you can rob = 1 + 3 = 4.
    Example 3:
    
    Input: nums = [1,2,3]
    Output: 3

---
+++ LeetCode/Assignment213.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Assignment213
+{
+    public class Solution
+    {
+        public int Rob(int[] nums)
+        {
+            if (nums.Length == 1)
+                return nums[0];
+
+            return Math.Max(RobRange(nums, 0, nums.Length - 2), RobRange(nums, 1, nums.Length - 1));
+
+            int RobRange(int[] nums, int start, int end)
+            {
+                var rob1 = 0;
+                var rob2 = 0;
+                for (int i = start; i <= end; i++)
+                {
+                    var temp = Math.Max(rob1 + nums[i], rob2);
+                    rob1 = rob2;
+                    rob2 = temp;
+                }
+                return rob2;
+            }
+        }
+    }
+}
