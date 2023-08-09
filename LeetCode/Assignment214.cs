993e6465f2f6faf74f739eae36cc6dbc9a2167f8
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-08-10 01:49:22 +0300

    Assignment214
    You are given a string s. You can convert s to a
    palindrome
     by adding characters in front of it.
    
    Return the shortest palindrome you can find by performing this transformation.
    
    Example 1:
    
    Input: s = "aacecaaa"
    Output: "aaacecaaa"
    Example 2:
    
    Input: s = "abcd"
    Output: "dcbabcd"

---
+++ LeetCode/Assignment214.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Assignment214
+{
+    public class Solution
+    {
+        public string ShortestPalindrome(string s)
+        {
+            int n = s.Length;
+            int i = 0;
+            for (int j = n - 1; j >= 0; j--)
+                if (s[i] == s[j])
+                    i++;
+            if (i == n)
+                return s;
+            string r = s.Substring(i, n - i);
+            string rev = r;
+
+            char[] ch = rev.ToCharArray();
+            Array.Reverse(ch);
+            rev = new string(ch);
+            Console.WriteLine(rev);
+            return rev + ShortestPalindrome(s.Substring(0, i)) + r;
+
+        }
+    }
+}
