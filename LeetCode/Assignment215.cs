77072d726ef23d36cca720049d50da3c7b93273f
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-08-11 22:56:23 +0300

    Assignment215
    Given an integer array nums and an integer k, return the kth largest element in the array.
    
    Note that it is the kth largest element in the sorted order, not the kth distinct element.
    
    Can you solve it without sorting?
    
    Example 1:
    
    Input: nums = [3,2,1,5,6,4], k = 2
    Output: 5
    Example 2:
    
    Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
    Output: 4

---
+++ LeetCode/Assignment215.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Assignment215
+{
+    public class Solution
+    {
+        public int FindKthLargest(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            return nums[nums.Length - k];
+        }
+    }
+}
