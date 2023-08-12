c2dbd61715d1380118230ffca7373920d0165185
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-08-12 12:14:28 +0300

    Assignment217
    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
    
    Example 1:
    
    Input: nums = [1,2,3,1]
    Output: true
    Example 2:
    
    Input: nums = [1,2,3,4]
    Output: false
    Example 3:
    
    Input: nums = [1,1,1,3,3,4,3,2,4,2]
    Output: true

---
+++ LeetCode/Assignment217.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Assignment217
+{
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            Array.Sort(nums);
+            int n = nums.Length;
+            for (int i = 0; i < n - 1; i++)
+                if (nums[i] == nums[i + 1])
+                    return true;
+
+            return false;
+        }
+    }
+}
