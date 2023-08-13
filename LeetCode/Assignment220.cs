17f367b2ba1ff9e930ec1fc2ee4a5bd47fe593ea
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-08-14 01:37:30 +0300

    Assignment220
    You are given an integer array nums and two integers indexDiff and valueDiff.
    
    Find a pair of indices (i, j) such that:
    
    i != j,
    abs(i - j) <= indexDiff.
    abs(nums[i] - nums[j]) <= valueDiff, and
    Return true if such pair exists or false otherwise.
    
    Example 1:
    
    Input: nums = [1,2,3,1], indexDiff = 3, valueDiff = 0
    Output: true
    Explanation: We can choose (i, j) = (0, 3).
    We satisfy the three conditions:
    i != j --> 0 != 3
    abs(i - j) <= indexDiff --> abs(0 - 3) <= 3
    abs(nums[i] - nums[j]) <= valueDiff --> abs(1 - 1) <= 0
    Example 2:
    
    Input: nums = [1,5,9,1,5,9], indexDiff = 2, valueDiff = 3
    Output: false
    Explanation: After trying all the possible pairs (i, j), we cannot satisfy the three conditions, so we return false.

---
+++ LeetCode/Assignment220.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment220
+{
+    public class Solution
+    {
+        public bool ContainsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff)
+        {
+
+            Dictionary<int, int> bucket = new Dictionary<int, int>();
+
+            int k = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                if (i > indexDiff)
+                {
+                    int partitionToDelete = nums[k] / (valueDiff + 1);
+
+                    if (nums[k] < 0)
+                        partitionToDelete = partitionToDelete - 1;
+
+                    bucket.Remove(partitionToDelete);
+                    k++;
+                }
+
+                int partition = nums[i] / (valueDiff + 1);
+                if (nums[i] < 0)
+                    partition = partition - 1;
+
+                if (bucket.ContainsKey(partition) || (bucket.ContainsKey(partition - 1) && Math.Abs(bucket[partition - 1] - nums[i]) <= valueDiff)
+                || (bucket.ContainsKey(partition + 1) && Math.Abs(bucket[partition + 1] - nums[i]) <= valueDiff))
+                    return true;
+                else
+                    bucket.Add(partition, nums[i]);
+            }
+
+            return false;
+        }
+    }
+}
