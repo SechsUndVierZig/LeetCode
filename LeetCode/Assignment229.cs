49bf3ab8ab0ab55a1a57dfd9f3e0038b6d7a63af
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-08-25 13:17:48 +0300

    Assignment229
    Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
    
    Example 1:
    
    Input: nums = [3,2,3]
    Output: [3]
    Example 2:
    
    Input: nums = [1]
    Output: [1]
    Example 3:
    
    Input: nums = [1,2]
    Output: [1,2]

---
+++ LeetCode/Assignment229.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment229
+{
+    public class Solution
+    {
+        public IList<int> MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> arr = new Dictionary<int, int>();
+            List<int> ans = new List<int>();
+            int x = 0;
+            foreach (int a in nums)
+            {
+                if (arr.TryAdd(a, 1) == false)
+                    arr[a]++;
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                arr.TryGetValue(nums[i], out x);
+                if (x > nums.Length / 3)
+                    ans.Add(nums[i]);
+            }
+            return (ans.Distinct().ToArray());
+        }
+    }
+}
