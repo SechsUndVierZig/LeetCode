6ce7c93da2a1be23df38e3b903cabbf65943e300
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-08-29 14:44:52 +0300

    Assignment232
    Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
    
    Implement the MyQueue class:
    
    void push(int x) Pushes element x to the back of the queue.
    int pop() Removes the element from the front of the queue and returns it.
    int peek() Returns the element at the front of the queue.
    boolean empty() Returns true if the queue is empty, false otherwise.
    Notes:
    
    You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
    Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
    
    Example 1:
    
    Input
    ["MyQueue", "push", "push", "peek", "pop", "empty"]
    [[], [1], [2], [], [], []]
    Output
    [null, null, null, 1, 1, false]
    
    Explanation
    MyQueue myQueue = new MyQueue();
    myQueue.push(1); // queue is: [1]
    myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
    myQueue.peek(); // return 1
    myQueue.pop(); // return 1, queue is [2]
    myQueue.empty(); // return false

---
+++ LeetCode/Assignment232.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Assignment232
+{
+    public class MyQueue
+    {
+
+        private Stack<int> Stack;
+        private Stack<int> StackToImpelemnt;
+        public MyQueue()
+        {
+            Stack = new Stack<int>();
+            StackToImpelemnt = new Stack<int>();
+        }
+
+        public void Push(int x)
+        {
+            Stack.Push(x);
+        }
+
+        public int Pop()
+        {
+            while (Stack.Count > 0)
+            {
+                StackToImpelemnt.Push(Stack.Pop());
+            }
+            int poppedOut = StackToImpelemnt.Pop();
+            while (StackToImpelemnt.Count > 0)
+            {
+                Stack.Push(StackToImpelemnt.Pop());
+            }
+            return poppedOut;
+        }
+
+        public int Peek()
+        {
+            while (Stack.Count > 0)
+            {
+                StackToImpelemnt.Push(Stack.Pop());
+            }
+            int peek = StackToImpelemnt.Peek();
+            while (StackToImpelemnt.Count > 0)
+            {
+                Stack.Push(StackToImpelemnt.Pop());
+            }
+            return peek;
+        }
+
+        public bool Empty()
+        {
+            return Stack.Count == 0;
+        }
+    }
+
+    /**
+     * Your MyQueue object will be instantiated and called as such:
+     * MyQueue obj = new MyQueue();
+     * obj.Push(x);
+     * int param_2 = obj.Pop();
+     * int param_3 = obj.Peek();
+     * bool param_4 = obj.Empty();
+     */
+}
