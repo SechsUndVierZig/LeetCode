d20d5f8ac7f460a390596974f48f8e76e562e3d1
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-08-30 15:21:59 +0300

    Assignment233
    Given an integer n, count the total number of digit 1 appearing in all non-negative integers less than or equal to n.
    
    Example 1:
    
    Input: n = 13
    Output: 6
    Example 2:
    
    Input: n = 0
    Output: 0

---
+++ LeetCode/Assignment233.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Assignment233
+{
+    public class Solution
+    {
+        public int CountDigitOne(int n)
+        {
+            int ones = 0, m = 1;
+            while (m <= n)
+            {
+                int a = n / m, b = n % m;
+                ones += (a + 8) / 10 * m;
+                if (a % 10 == 1) ones += b + 1;
+                m *= 10;
+            }
+            return ones;
+        }
+
+    }
+}
