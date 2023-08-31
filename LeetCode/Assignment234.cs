21ec03a0cd5e6640a7d386987824f3fbde6987ce
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-08-31 14:47:54 +0300

    Assignment234
    Given the head of a singly linked list, return true if it is a
    palindrome
     or false otherwise.
    
    Example 1:
    
    Input: head = [1,2,2,1]
    Output: true
    Example 2:
    
    Input: head = [1,2]
    Output: false

---
+++ LeetCode/Assignment234.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Assignment234
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+    public class Solution
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            bool isPali = true;
+
+            Stack ReverseNode = new Stack();
+            Queue StraightNode = new Queue();
+            ListNode currentNode = head;
+
+            while (currentNode != null)
+            {
+                ReverseNode.Push(currentNode.val);
+                StraightNode.Enqueue(currentNode.val);
+                currentNode = currentNode.next;
+            }
+
+            while (ReverseNode.Count > 0)
+            {
+                if (ReverseNode.Peek().Equals(StraightNode.Peek()))
+                {
+                    ReverseNode.Pop();
+                    StraightNode.Dequeue();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+
+            return isPali;
+        }
+    }
+}
