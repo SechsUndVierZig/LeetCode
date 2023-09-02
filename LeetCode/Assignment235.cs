ac8670b0daed8dd552b5509c1c7cc96707dd973f
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-09-02 15:12:10 +0300

    Assignment235
    Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
    
    According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
    
    Example 1:
    
    Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
    Output: 6
    Explanation: The LCA of nodes 2 and 8 is 6.
    Example 2:
    
    Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
    Output: 2
    Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
    Example 3:
    
    Input: root = [2,1], p = 2, q = 1
    Output: 2

---
+++ LeetCode/Assignment235.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Assignment235
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+
+    public class Solution
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            TreeNode curr = root;
+            while (curr != null)
+            {
+                if (p.val > curr.val && q.val > curr.val)
+                    curr = curr.right;
+                else if (p.val < curr.val && q.val < curr.val)
+                    curr = curr.left;
+                else
+                    break;
+            }
+            return curr;
+        }
+    }
+}
