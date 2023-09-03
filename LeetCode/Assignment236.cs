8124d70b810c1c02a5b2d1b9d651b44a7873fd64
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-09-03 14:39:49 +0300

    Assignment236
    Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
    
    According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
    
    Example 1:
    
    Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
    Output: 3
    Explanation: The LCA of nodes 5 and 1 is 3.
    Example 2:
    
    Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
    Output: 5
    Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
    Example 3:
    
    Input: root = [1,2], p = 1, q = 2
    Output: 1

---
+++ LeetCode/Assignment236.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Assignment236
+{
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+    public class Solution
+    {
+
+        bool found = false;
+        public void FindPath(TreeNode root, TreeNode p, List<TreeNode> list)
+        {
+            if (root == null) return;
+            list.Add(root);
+            if (root.val == p.val) found = true;
+            if (found != true)
+                FindPath(root.left, p, list);
+            if (found != true)
+                FindPath(root.right, p, list);
+            if (found != true)
+                list.Remove(root);
+        }
+
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            List<TreeNode> list1 = new List<TreeNode>();
+            FindPath(root, p, list1);
+            List<TreeNode> list2 = new List<TreeNode>();
+            found = false;
+            FindPath(root, q, list2);
+
+            if (list1.Count == 0 || list2.Count == 0) return null;
+
+            int count = Math.Min(list1.Count, list2.Count);
+            int i;
+            for (i = 0; i < count; i++)
+                if (list1[i] != list2[i]) break;
+
+            if (i - 1 < 0) return null;
+
+            return list1[i - 1];
+
+        }
+    }
+}
