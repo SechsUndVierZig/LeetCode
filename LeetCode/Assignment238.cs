d52fc62cb72375c45becb43ffd9334303b77d113
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-09-17 16:53:59 +0300

    Assignment238
    Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
    
    The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
    
    You must write an algorithm that runs in O(n) time and without using the division operation.
    
    Example 1:
    
    Input: nums = [1,2,3,4]
    Output: [24,12,8,6]
    Example 2:
    
    Input: nums = [-1,1,0,-3,3]
    Output: [0,0,9,0,0]

---
+++ LeetCode/Assignment238.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Assignment238
+{
+    public class Solution
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int number0 = 0;
+            int product = 1;
+            int[] answer = new int[nums.Length];
+
+            foreach (int num in nums)
+            {
+                if (num == 0)
+                {
+                    number0++;
+                    continue;
+                }
+                product *= num;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (number0 > 0 && nums[i] != 0 || number0 >= 2 && nums[i] == 0) answer[i] = 0;
+                else if (number0 == 1 && nums[i] == 0) answer[i] = product;
+                else answer[i] = product / nums[i];
+            }
+
+            return answer;
+        }
+    }
+}
