b8ba43b377c09666b8a1814ae7e6c859a65af20e
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-09-20 16:03:11 +0300

    Assignment241
    Given a string expression of numbers and operators, return all possible results from computing all the different possible ways to group numbers and operators. You may return the answer in any order.
    
    The test cases are generated such that the output values fit in a 32-bit integer and the number of different results does not exceed 104.
    
    Example 1:
    
    Input: expression = "2-1-1"
    Output: [0,2]
    Explanation:
    ((2-1)-1) = 0
    (2-(1-1)) = 2
    Example 2:
    
    Input: expression = "2*3-4*5"
    Output: [-34,-14,-10,-10,10]
    Explanation:
    (2*(3-(4*5))) = -34
    ((2*3)-(4*5)) = -14
    ((2*(3-4))*5) = -10
    (2*((3-4)*5)) = -10
    (((2*3)-4)*5) = 10

---
+++ LeetCode/Assignment241.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Assignment241
+{
+    public class Solution
+    {
+        public IList<int> DiffWaysToCompute(string expression)
+        {
+            var cache = new Dictionary<(int, int), List<int>>();
+            var numbers = expression.Split("+-*".ToCharArray()).Select(int.Parse).ToArray();
+            var ops = expression.Where(c => !Char.IsDigit(c)).ToArray();
+            return Compute(0, numbers.Length - 1);
+            IList<int> Compute(int start, int end)
+            {
+                if (cache.TryGetValue((start, end), out var subResult))
+                    return subResult;
+
+                subResult = new List<int>();
+
+                if (start == end)
+                    subResult.Add(numbers[start]);
+                else
+                {
+                    for (var i = start + 1; i <= end; i++)
+                    {
+                        var lpart = Compute(start, i - 1);
+                        var rpart = Compute(i, end);
+                        Func<int, int, int> func = ops[i - 1] switch
+                        {
+                            '+' => (a, b) => a + b,
+                            '-' => (a, b) => a - b,
+                            _ => (a, b) => a * b
+                        };
+
+                        foreach (var ln in lpart)
+                            foreach (var rn in rpart)
+                                subResult.Add(func(ln, rn));
+                    }
+                }
+                cache.Add((start, end), subResult);
+                return subResult;
+            }
+        }
+    }
+}
